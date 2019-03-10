eca851da8ea91ce0aca450f0fd4155560ba92d06
peterpeterparker <david.dalbusco@outlook.com>
2019-03-10 14:32:34 +0100

    feat: development, no service worker

--- studio/stencil.config.ts
+++ studio/stencil.config.ts
@@ -5,22 +5,23 @@ import {postcss} from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
 
 export const config: Config = {
-  outputTargets: [{
-      type: 'www'
-  }],
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.scss',
-  plugins: [
-    sass(),
-    postcss({
-      plugins: [autoprefixer()]
-    })
-  ],
-  nodeResolve: {browser: true},
-  devServer: {
-    openBrowser: false
-  },
-  copy: [
-    {src: 'robots.txt'}
-  ]
+    outputTargets: [{
+        type: 'www',
+        serviceWorker: null
+    }],
+    globalScript: 'src/global/app.ts',
+    globalStyle: 'src/global/app.scss',
+    plugins: [
+        sass(),
+        postcss({
+            plugins: [autoprefixer()]
+        })
+    ],
+    nodeResolve: {browser: true},
+    devServer: {
+        openBrowser: false
+    },
+    copy: [
+        {src: 'robots.txt'}
+    ]
 };
