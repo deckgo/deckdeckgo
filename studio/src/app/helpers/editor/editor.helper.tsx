136b6bc35877b414df8a4a1dfe147d45f6958d36
peterpeterparker <david.dalbusco@outlook.com>
2019-03-17 11:19:16 +0100

    feat: create or update deck

--- studio/src/app/helpers/editor/editor.helper.tsx
+++ studio/src/app/helpers/editor/editor.helper.tsx
@@ -1,18 +1,22 @@
 import {SlideTemplate} from '../../models/slide-template';
 import {Slide} from '../../models/slide';
+import {Deck} from '../../models/deck';
 
 import {SlideService} from '../../services/slide/slide.service';
+import {DeckService} from '../../services/deck/deck.service';
 
 export class EditorHelper {
 
     private el: HTMLElement;
 
     private slideService: SlideService;
-    // private deckService: DeckService;
+    private deckService: DeckService;
+
+    private deck: Deck;
 
     constructor() {
         this.slideService = SlideService.getInstance();
-        // this.deckService = DeckService.getInstance();
+        this.deckService = DeckService.getInstance();
     }
 
     init(el: HTMLElement) {
@@ -52,6 +56,8 @@ export class EditorHelper {
 
                 if (persistedSlide && persistedSlide.slide_id) {
                     slide.setAttribute('slide_id', persistedSlide.slide_id);
+
+                    await this.createOrUpdateDeck(persistedSlide);
                 }
             } catch (err) {
                 // TODO do something with the error
@@ -61,5 +67,37 @@ export class EditorHelper {
         });
     }
 
+    private createOrUpdateDeck(slide: Slide): Promise<void> {
+        return new Promise<void>(async (resolve, reject) => {
+            try {
+                if (!slide) {
+                    reject('Missing slide to create or update the deck');
+                    return;
+                }
+
+                if (this.deck) {
+                    if (!this.deck.deck_slides || this.deck.deck_slides.length <= 0) {
+                        this.deck.deck_slides = [];
+                    }
+
+                    this.deck.deck_slides.push(slide.slide_id);
+
+                    this.deck = await this.deckService.put(this.deck);
+                } else {
+                    this.deck = {
+                        deck_slides: [slide.slide_id]
+                    };
+
+                    this.deck = await this.deckService.post(this.deck);
+                }
+
+                console.log(this.deck);
+
+                resolve();
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
 
 }
--- studio/src/app/services/deck/deck.service.tsx
+++ studio/src/app/services/deck/deck.service.tsx
@@ -1,3 +1,7 @@
+import {EnvironmentConfigService} from '../environment/environment-config.service';
+
+import {Deck} from '../../models/deck';
+
 export class DeckService {
 
     private static instance: DeckService;
@@ -11,6 +15,39 @@ export class DeckService {
             DeckService.instance = new DeckService();
         }
         return DeckService.instance;
+    }
+
+    post(deck: Deck): Promise<Deck> {
+        return this.postOrPut(deck, 'POST');
+    }
+
+    put(deck: Deck): Promise<Deck> {
+        return this.postOrPut(deck, 'PUT');
+    }
+
+    private postOrPut(deck: Deck, method: string): Promise<Deck> {
+        return new Promise<Deck>(async (resolve, reject) => {
+            try {
+                const apiUrl: string = EnvironmentConfigService.getInstance().get('apiUrl');
+
+                const rawResponse: Response = await fetch(apiUrl + '/decks', {
+                    method: method,
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(deck)
+                });
+
+                const persistedDeck: Deck = await rawResponse.json();
+
+                console.log(persistedDeck);
+
+                resolve(persistedDeck);
+            } catch (err) {
+                reject(err);
+            }
+        });
     }
 }
 
