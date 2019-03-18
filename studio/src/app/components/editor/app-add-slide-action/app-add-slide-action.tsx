71ca251f28aa99eb89a7213433980196b680cb04
peterpeterparker <david.dalbusco@outlook.com>
2019-03-18 21:07:12 +0100

    feat: if deck is busy, can't add slide

--- studio/src/app/components/editor/app-add-slide-action/app-add-slide-action.tsx
+++ studio/src/app/components/editor/app-add-slide-action/app-add-slide-action.tsx
@@ -1,4 +1,8 @@
-import {Component, Event, EventEmitter} from '@stencil/core';
+import {Component, Event, EventEmitter, State} from '@stencil/core';
+
+import {Subscription} from 'rxjs';
+
+import {DeckBusyService} from '../../../services/deck/deck-busy.service';
 
 @Component({
     tag: 'app-add-slide-action',
@@ -9,12 +13,34 @@ export class AppAddSlideAction {
 
     @Event() private actionOpenSlideAdd: EventEmitter<UIEvent>;
 
+    private subscription: Subscription;
+    private deckBusyService: DeckBusyService;
+
+    @State()
+    private deckBusy: boolean = false;
+
+    constructor() {
+        this.deckBusyService = DeckBusyService.getInstance();
+    }
+
     private openSlideAdd($event: UIEvent) {
         this.actionOpenSlideAdd.emit($event);
     }
 
+    async componentWillLoad() {
+        this.subscription = this.deckBusyService.watch().subscribe((busy: boolean) => {
+            this.deckBusy = busy;
+        });
+    }
+
+    async componentDidUnload() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
     render() {
-        return <ion-button onClick={(e: UIEvent) => this.openSlideAdd(e)} color="primary" shape="round"
+        return <ion-button onClick={(e: UIEvent) => this.openSlideAdd(e)} color="primary" shape="round" disabled={this.deckBusy}
                            size="small">
             <ion-label>Add slide</ion-label>
         </ion-button>;
