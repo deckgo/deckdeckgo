d232c8421f0f34d3671f99c3d4d6f688957e6efb
peterpeterparker <david.dalbusco@outlook.com>
2019-04-12 10:00:27 +0200

    feat: disconnect observer once loaded

--- studio/src/app/components/feed/card/app-feed-lazy-img/app-feed-lazy-img.tsx
+++ studio/src/app/components/feed/card/app-feed-lazy-img/app-feed-lazy-img.tsx
@@ -9,18 +9,25 @@ export class AppFeedLazyImg {
 
     @Element() el: HTMLElement;
 
+    private observer: IntersectionObserver;
+
     async componentDidLoad() {
         const img: HTMLImageElement = this.el.shadowRoot.querySelector('img');
 
         if (img) {
-            const observer: IntersectionObserver = new IntersectionObserver(this.onIntersection, { rootMargin: '100px 0px' });
-            observer.observe(img);
+            this.observer = new IntersectionObserver(this.onIntersection, { rootMargin: '100px 0px' });
+            this.observer.observe(img);
         }
     }
 
     private onIntersection = async (entries: IntersectionObserverEntry[]) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
+
+                if (this.observer) {
+                    this.observer.disconnect();
+                }
+
                 if (entry.target.getAttribute('data-src')) {
                     entry.target.setAttribute('src', entry.target.getAttribute('data-src'));
                     entry.target.removeAttribute('data-src');
