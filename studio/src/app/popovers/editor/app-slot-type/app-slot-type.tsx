c69122733f9607f7d0a93e67e3aed961e8973ecd
peterpeterparker <david.dalbusco@outlook.com>
2019-04-24 21:21:51 +0200

    fix: don't change slot type if selected is the same as the current one

--- studio/src/app/popovers/editor/app-slot-type/app-slot-type.tsx
+++ studio/src/app/popovers/editor/app-slot-type/app-slot-type.tsx
@@ -24,7 +24,7 @@ export class AppSlideAdd {
 
     async closePopover(type: SlotType) {
         await (this.el.closest('ion-popover') as HTMLIonModalElement).dismiss({
-            type: type
+            type: this.currentType !== type ? type : null
         });
     }
 
