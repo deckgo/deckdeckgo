fbc70677f0fab75f91f853b1b73e2faa4dd9b02f
peterpeterparker <david.dalbusco@outlook.com>
2019-05-19 15:56:03 +0200

    feat(#125): add input for the username

--- studio/src/app/pages/core/app-settings/app-settings.tsx
+++ studio/src/app/pages/core/app-settings/app-settings.tsx
@@ -1,8 +1,10 @@
-import {Component, State} from '@stencil/core';
+import {Component, Listen, State} from '@stencil/core';
 import {filter, take} from 'rxjs/operators';
 
 import {AuthUser} from '../../../models/auth-user';
+import {User} from '../../../models/user';
 
+import {UserService} from '../../../services/api/user/user.service';
 import {AuthService} from '../../../services/api/auth/auth.service';
 import {NavDirection, NavService} from '../../../services/core/nav/nav.service';
 
@@ -15,12 +17,20 @@ export class AppHome {
     @State()
     private authUser: AuthUser;
 
+    @State()
+    private user: User;
+
+    @State()
+    private valid: boolean = true;
+
     private authService: AuthService;
+    private userService: UserService;
 
     private navService: NavService;
 
     constructor() {
         this.authService = AuthService.getInstance();
+        this.userService= UserService.getInstance();
         this.navService = NavService.getInstance();
     }
 
@@ -30,6 +40,17 @@ export class AppHome {
             take(1)).subscribe(async (authUser: AuthUser) => {
                 this.authUser = authUser;
         });
+
+        this.userService.watch().pipe(
+            filter((user: User) => user !== null && user !== undefined),
+            take(1)).subscribe(async (user: User) => {
+                this.user = user;
+        });
+    }
+
+    @Listen('keydown.enter')
+    handleEnterKey() {
+        // TODO submit
     }
 
     private async signIn() {
@@ -37,6 +58,21 @@ export class AppHome {
             url: '/signin' + (window && window.location ? window.location.pathname : ''),
             direction: NavDirection.FORWARD
         });
+    }
+
+    private handleSubmit(e: Event) {
+        e.preventDefault();
+
+        console.log(e);
+        // send data to our backend
+    }
+
+    private handleUsernameInput($event: CustomEvent<KeyboardEvent>) {
+        this.user.username = ($event.target as InputTargetEvent).value;
+    }
+
+    private validateUsernameInput() {
+        this.valid = this.user && this.user.username && this.user.username !== null && this.user.username !== undefined && this.user.username.length >= 3 && this.user.username.length <= 32;
     }
 
     render() {
@@ -54,7 +90,7 @@ export class AppHome {
         if (!this.authUser) {
             return this.renderNotLoggedInContent();
         } else{
-            return <h1>Settings</h1>
+            return this.renderUserContent();
         }
     }
 
@@ -63,5 +99,43 @@ export class AppHome {
             <h1>Oh, hi! Good to have you.</h1>,
             <p class="ion-padding-top"><ion-anchor onClick={() => this.signIn()}>Sign in</ion-anchor> to access your settings.</p>
         ]
+    }
+
+    private renderUserContent() {
+        return [
+            <h1>Settings</h1>,
+            <form onSubmit={(e: Event) => this.handleSubmit(e)}>
+                <ion-list>
+                    {this.renderUsername()}
+                </ion-list>
+
+                {this.renderSubmitForm()}
+            </form>
+        ]
+    }
+
+    private renderUsername() {
+        if (this.user) {
+            return [<ion-item class="item-title">
+                <ion-label>Username</ion-label>
+                </ion-item>,
+                <ion-item>
+                    <ion-input value={this.user.username} debounce={500} minlength={3} maxlength={32} required={true} input-mode="text"
+                           onIonInput={(e: CustomEvent<KeyboardEvent>) => this.handleUsernameInput(e)}
+                           onIonChange={() => this.validateUsernameInput()}></ion-input>
+            </ion-item>];
+        } else {
+            return undefined;
+        }
+    }
+
+    private renderSubmitForm() {
+        if (this.user) {
+            return <ion-button type="submit" disabled={!this.valid} color="primary" shape="round" class="ion-margin-top">
+                <ion-label>Submit</ion-label>
+            </ion-button>
+        } else {
+            return undefined;
+        }
     }
 }
