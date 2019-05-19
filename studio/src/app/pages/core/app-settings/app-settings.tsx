6c43ecf29e075190e35a37ad96d04817ab474d22
peterpeterparker <david.dalbusco@outlook.com>
2019-05-19 12:14:49 +0200

    feat(#125): guard if not logged in

--- studio/src/app/pages/core/app-settings/app-settings.tsx
+++ studio/src/app/pages/core/app-settings/app-settings.tsx
@@ -1,4 +1,10 @@
-import {Component} from '@stencil/core';
+import {Component, State} from '@stencil/core';
+import {filter, take} from 'rxjs/operators';
+
+import {AuthUser} from '../../../models/auth-user';
+
+import {AuthService} from '../../../services/api/auth/auth.service';
+import {NavDirection, NavService} from '../../../services/core/nav/nav.service';
 
 @Component({
     tag: 'app-settings',
@@ -6,17 +12,56 @@ import {Component} from '@stencil/core';
 })
 export class AppHome {
 
+    @State()
+    private authUser: AuthUser;
+
+    private authService: AuthService;
+
+    private navService: NavService;
+
+    constructor() {
+        this.authService = AuthService.getInstance();
+        this.navService = NavService.getInstance();
+    }
+
+    componentWillLoad() {
+        this.authService.watch().pipe(
+            filter((authUser: AuthUser) => authUser !== null && authUser !== undefined),
+            take(1)).subscribe(async (authUser: AuthUser) => {
+                this.authUser = authUser;
+        });
+    }
+
+    private async signIn() {
+        this.navService.navigate({
+            url: '/signin' + (window && window.location ? window.location.pathname : ''),
+            direction: NavDirection.FORWARD
+        });
+    }
+
     render() {
         return [
             <app-navigation></app-navigation>,
             <ion-content class="ion-padding fullscreen-padding">
                 <main padding>
-
-                    <h1>Hello</h1>
-
-
+                    {this.renderGuardedContent()}
                 </main>
             </ion-content>
         ];
+    }
+
+    private renderGuardedContent() {
+        if (!this.authUser) {
+            return this.renderNotLoggedInContent();
+        } else{
+            return <h1>Settings</h1>
+        }
+    }
+
+    private renderNotLoggedInContent() {
+        return [
+            <h1>Oh, hi! Good to have you.</h1>,
+            <p class="ion-padding-top"><ion-anchor onClick={() => this.signIn()}>Sign in</ion-anchor> to access your settings.</p>
+        ]
     }
 }
