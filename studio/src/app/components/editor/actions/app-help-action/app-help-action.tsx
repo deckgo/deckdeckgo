695439b1a548def80473f99a2693a63fe9759499
peterpeterparker <david.dalbusco@outlook.com>
2019-07-12 13:58:47 +0200

    style: help icon size

--- studio/src/app/components/editor/actions/app-help-action/app-help-action.tsx
+++ studio/src/app/components/editor/actions/app-help-action/app-help-action.tsx
@@ -39,7 +39,7 @@ export class AppHelpAction {
 
     render() {
         return <ion-tab-button onClick={() => this.openGetHelp()} color="primary" mode="md" class="get-help-action">
-            <ion-icon name="help"></ion-icon>
+            <ion-icon name="help" md="md-help" ios="md-help"></ion-icon>
             <ion-label>Help</ion-label>
         </ion-tab-button>
     }
