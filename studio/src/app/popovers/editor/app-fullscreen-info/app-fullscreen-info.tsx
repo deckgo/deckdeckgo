6093b0c76b2556a624fe522f3f9579ace407d035
peterpeterparker <david.dalbusco@outlook.com>
2019-07-12 15:05:50 +0200

    fix: typo

--- studio/src/app/popovers/editor/app-fullscreen-info/app-fullscreen-info.tsx
+++ studio/src/app/popovers/editor/app-fullscreen-info/app-fullscreen-info.tsx
@@ -14,8 +14,8 @@ export class AppFullscreenInfo {
     render() {
         return <div class="ion-padding">
             <h2>Tips about fullscreen</h2>
-            <p>Cool thing, with DeckDeckGo you could edit your presentation in full screen mode too!</p>
-            <p>Correct typo, add slide, change colors etc. all features are also available in full screen mode 😉</p>
+            <p>Cool thing, with DeckDeckGo you can edit your presentation in full screen mode too!</p>
+            <p>Correct typo, add slides, change colors etc. all features are also available in fullscreen mode 😉</p>
             <div class="ion-text-center ion-padding-top"><ion-button size="small" shape="round" color="primary" onClick={() => this.closePopover()}>Got it</ion-button></div>
         </div>
     }
