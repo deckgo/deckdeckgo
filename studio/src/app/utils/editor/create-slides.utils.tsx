26fdae9f4b1f2c544f18cfca510d88ca6a784a9b
peterpeterparker <david.dalbusco@outlook.com>
2019-07-17 07:32:06 +0200

    feat: update lazy-img to parse SVG

--- studio/src/app/utils/editor/create-slides.utils.tsx
+++ studio/src/app/utils/editor/create-slides.utils.tsx
@@ -160,42 +160,42 @@ export class CreateSlidesUtils {
 
                 if (userSocial.twitter && userSocial.twitter !== '' && userSocial.twitter !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} twitter={user.data.social.twitter}>
-                        <deckgo-lazy-img img-src="/assets/icons/ionicons/twitter.svg" img-alt="Twitter"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/ionicons/twitter.svg" aria-label="Twitter"></deckgo-lazy-img>
                         Twitter
                     </deckgo-social>);
                 }
 
                 if (userSocial.linkedin && userSocial.linkedin !== '' && userSocial.linkedin !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} linkedin={user.data.social.linkedin}>
-                        <deckgo-lazy-img img-src="/assets/icons/ionicons/linkedin.svg" img-alt="LinkedIn"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/ionicons/linkedin.svg" aria-label="LinkedIn"></deckgo-lazy-img>
                         LinkedIn
                     </deckgo-social>);
                 }
 
                 if (userSocial.dev && userSocial.dev !== '' && userSocial.dev !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} dev={user.data.social.dev}>
-                        <deckgo-lazy-img img-src="/assets/icons/dev.svg" img-alt="Dev"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/dev.svg" aria-label="Dev"></deckgo-lazy-img>
                         Dev
                     </deckgo-social>);
                 }
 
                 if (userSocial.medium && userSocial.medium !== '' && userSocial.medium !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} medium={user.data.social.medium}>
-                        <deckgo-lazy-img img-src="/assets/icons/medium.svg" img-alt="Medium"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/medium.svg" aria-label="Medium"></deckgo-lazy-img>
                         Medium
                     </deckgo-social>);
                 }
 
                 if (userSocial.github && userSocial.github !== '' && userSocial.github !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} github={user.data.social.github}>
-                        <deckgo-lazy-img img-src="/assets/icons/ionicons/github.svg" img-alt="GitHub"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/ionicons/github.svg" aria-label="GitHub"></deckgo-lazy-img>
                         GitHub
                     </deckgo-social>);
                 }
 
                 if (userSocial.custom && userSocial.custom !== '' && userSocial.custom !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} fullUrl={user.data.social.custom}>
-                        <deckgo-lazy-img img-src="/assets/icons/ionicons/globe.svg" img-alt="Web"></deckgo-lazy-img>
+                        <deckgo-lazy-img svg-src="/assets/icons/ionicons/globe.svg" aria-label="Web"></deckgo-lazy-img>
                         Web
                     </deckgo-social>);
                 }
