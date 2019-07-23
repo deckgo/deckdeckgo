1904f9888f43e8977f61664c069709f4a6b06eef
Nicolas Mattia <nicolas@nmattia.com>
2019-07-23 10:27:19 +0200

    infra: use S3 for terraform remote state

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,4 +1,12 @@
 
 terraform {
   required_version = ">= 0.12"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "deckgo-beta-terraform-state"
+    key            = "terraform.tfstate"
+    dynamodb_table = "deckgo-beta-terraform-state-lock"
+    encrypt        = true
+  }
 }
