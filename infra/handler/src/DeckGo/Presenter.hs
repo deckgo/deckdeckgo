100bb7d19082c5a5949a1ff306d4582d874b3c2a
Nicolas Mattia <nicolas@nmattia.com>
2019-07-28 17:17:25 +0200

    infra: more haskell cleanup

--- infra/handler/src/DeckGo/Presenter.hs
+++ infra/handler/src/DeckGo/Presenter.hs
@@ -17,7 +17,6 @@ import Data.Function
 import Data.List (foldl')
 import Data.Maybe
 import Data.String
--- import Data.Time.Clock (getCurrentTime)
 import DeckGo.Handler
 import DeckGo.Prelude
 import System.Environment
@@ -35,7 +34,6 @@ import qualified Hasql.Connection as HC
 import qualified Network.AWS as AWS
 import qualified Network.AWS.Data.Body as Body
 import qualified Network.AWS.S3 as S3
--- import qualified Network.AWS.CloudFront as CloudFront
 import qualified Network.Mime as Mime
 import qualified System.Directory as Dir
 import qualified System.IO.Temp as Temp
