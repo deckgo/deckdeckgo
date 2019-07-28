3ac1aa1b9e67099988e55ead8d62af3d0ba794e2
peterpeterparker <david.dalbusco@outlook.com>
2019-07-28 10:00:38 +0200

    feat: add link to start a new presentation in user menu

--- studio/src/app/popovers/core/app-user-menu/app-user-menu.tsx
+++ studio/src/app/popovers/core/app-user-menu/app-user-menu.tsx
@@ -40,21 +40,35 @@ export class AppUserMenu {
         await (this.el.closest('ion-popover') as HTMLIonModalElement).dismiss();
     }
 
+    private async navigateEditor() {
+        this.navService.navigate({
+            url: '/editor',
+            direction: NavDirection.ROOT
+        });
+
+        await this.closePopover();
+    }
+
     render() {
         return [
             <app-user-info></app-user-info>,
+            <hr/>,
             this.renderActions()
         ]
     }
 
     private renderActions() {
         return <ion-list>
+            <ion-item onClick={() => this.navigateEditor()}>
+                <ion-label>Write a presentation</ion-label>
+            </ion-item>
+
             <ion-item onClick={() => this.closePopover()}>
                 <ion-router-link href="/settings" routerDirection="forward"><ion-label>Settings</ion-label></ion-router-link>
             </ion-item>
 
             <ion-item onClick={() => this.signOut()}>
-                <ion-router-link><ion-label>Sign out</ion-label></ion-router-link>
+                <ion-label>Sign out</ion-label>
             </ion-item>
         </ion-list>
     }
