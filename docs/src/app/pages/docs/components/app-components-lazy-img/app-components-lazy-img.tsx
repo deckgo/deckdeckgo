f76fd4fc262fc2f7ec546dd7139f294cc1bccbf6
peterpeterparker <david.dalbusco@outlook.com>
2019-08-02 13:30:41 +0200

    feat: expose object-fit property of the image

--- docs/src/app/pages/docs/components/app-components-lazy-img/app-components-lazy-img.tsx
+++ docs/src/app/pages/docs/components/app-components-lazy-img/app-components-lazy-img.tsx
@@ -188,6 +188,11 @@ export class AppComponentsLazyImg {
 <td></td>
 <td>In case you would like to specify a border radius for the image</td>
 </tr>
+<tr>
+<td>--deckgo-lazy-img-object-fit</td>
+<td></td>
+<td>The property object-fit of the image</td>
+</tr>
 </tbody></table>
 <h3 id="app-components-lazy-img-methods">Methods</h3>
 <p>This component also export an async method <code>lazyLoad()</code> in case you would like to trigger &quot;manually&quot; the loading of the image.</p>
