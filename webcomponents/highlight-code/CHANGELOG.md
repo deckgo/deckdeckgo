232ea019edbf6898147ca20100baa027a61ed062
peterpeterparker <david.dalbusco@outlook.com>
2019-08-09 12:15:32 +0200

    feat: improve lines highlighting detection with ResizeObserver for modern browsers

--- webcomponents/highlight-code/CHANGELOG.md
+++ webcomponents/highlight-code/CHANGELOG.md
@@ -1,6 +1,10 @@
 <a name="1.0.0-alpha.18"></a>
 # 1.0.0-alpha.18 (Not released yet)
 
+## Features
+
+* improve lines highlighting detection with ResizeObserver for modern browsers
+
 ## Fix
 
 * the component displayed line numbers from 1 but the highlight used to expect line numbers from 0. this is now fixed, highlight will also begin with 1.
--- webcomponents/highlight-code/src/components/highlight-code/deckdeckgo-highlight-code.tsx
+++ webcomponents/highlight-code/src/components/highlight-code/deckdeckgo-highlight-code.tsx
@@ -190,7 +190,12 @@ export class DeckdeckgoHighlightCode {
             // split the code on linebreaks
             const regEx = RegExp(/\n(?!$)/g); //
             const match = code.split(regEx);
-            match.forEach(m => {
+            match.forEach((m, idx, array) => {
+              // On last element
+              if (idx === array.length - 1){
+                this.attachHighlightObserver(container);
+              }
+
               let div: HTMLElement = document.createElement('div');
               div.classList.add('deckgo-highlight-code-line-number');
 
@@ -202,14 +207,12 @@ export class DeckdeckgoHighlightCode {
               container.children[0].appendChild(div);
             });
           }else{
+            this.attachHighlightObserver(container);
+
             container.children[0].innerHTML = Prism.highlight(code, Prism.languages[this.language], this.language);
           }
 
           await this.addAnchors();
-
-          setTimeout(async () => {
-            await this.addHighlight();
-          }, 100);
 
           resolve();
         } catch (err) {
@@ -217,6 +220,24 @@ export class DeckdeckgoHighlightCode {
         }
       }
     });
+  }
+
+  private attachHighlightObserver(container: HTMLElement) {
+    if (window && 'ResizeObserver' in window) {
+      // @ts-ignore
+      const observer: ResizeObserver = new ResizeObserver(async (_entries) => {
+        await this.addHighlight();
+
+        observer.disconnect();
+      });
+
+      observer.observe(container);
+    } else {
+       // Back in my days...
+      setTimeout(async () => {
+        await this.addHighlight();
+      }, 100);
+    }
   }
 
   private addAnchors(): Promise<void> {
@@ -256,6 +277,7 @@ export class DeckdeckgoHighlightCode {
       if (this.highlightLines && this.highlightLines.length > 0) {
 
         const rows: number[] = await this.findRowsToHighlight();
+
         if (rows && rows.length > 0) {
           const containerCode: HTMLElement = this.el.shadowRoot.querySelector('code');
 
