af1634a594f5a86b571f872a4cc96244a999cbf6
peterpeterparker <david.dalbusco@outlook.com>
2019-08-11 19:19:44 +0200

    feat(#224): reveal on in the editor only user is presenting is deck aka not editing

--- studio/src/app/pages/editor/app-editor/app-editor.tsx
+++ studio/src/app/pages/editor/app-editor/app-editor.tsx
@@ -530,7 +530,7 @@ export class AppEditor {
 
                     {this.renderLoading()}
 
-                    <deckgo-deck embedded={true} style={this.style} reveal={this.fullscreen}
+                    <deckgo-deck embedded={true} style={this.style} reveal={this.fullscreen && this.presenting}
                                  onMouseDown={(e: MouseEvent) => this.deckTouched(e)}
                                  onTouchStart={(e: TouchEvent) => this.deckTouched(e)}
                                  onSlideNextDidChange={() => this.onSlideChangeHideToolbar()}
