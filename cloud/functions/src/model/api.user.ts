4e9ebf495454b4f9274b0699c46171ed88899304
peterpeterparker <david.dalbusco@outlook.com>
2019-09-06 15:59:50 +0200

    feat(#310): we are going to use the user id as storage root folder

--- cloud/functions/src/model/api.user.ts
+++
@@ -1,3 +0,0 @@
-export interface ApiUser {
-    username?: string;
-}
--- cloud/functions/src/watch/delete-user-storage.ts
+++ cloud/functions/src/watch/delete-user-storage.ts
@@ -3,8 +3,6 @@ import {DocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-import {ApiUser} from '../model/api.user';
-
 export async function deleteUserStorage(snap: DocumentSnapshot, context: functions.EventContext) {
     const userId: string = context.params.userId;
 
@@ -13,52 +11,22 @@ export async function deleteUserStorage(snap: DocumentSnapshot, context: functio
     }
 
     try {
-        const username: string | null = await findUserName(userId);
-
-        await deleteStorage(username);
+        await deleteStorage(userId);
     } catch (err) {
         console.error(err);
     }
 }
 
-function findUserName(userId: string): Promise<string | null> {
-    return new Promise<string | null>(async (resolve, reject) => {
-        try {
-            const apiUrl: string = functions.config().api.url;
-
-            const rawResponse: Response = await fetch(apiUrl + `/users/${userId}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (!rawResponse || !rawResponse.ok) {
-                // 404 if not found
-                resolve(null);
-                return;
-            }
-
-            const persistedUser: ApiUser = await rawResponse.json();
-
-            resolve(persistedUser ? persistedUser.username : null);
-        } catch (err) {
-            reject(err);
-        }
-    });
-}
-
-function deleteStorage(username: string | null): Promise<void> {
+function deleteStorage(userId: string | null): Promise<void> {
     return new Promise<void>(async (resolve, reject) => {
         try {
-            if (!username || username === undefined || username === '') {
+            if (!userId || userId === undefined || userId === '') {
                 resolve();
                 return;
             }
 
             const bucket = admin.storage().bucket();
-            await bucket.deleteFiles({prefix: `${username}/`});
+            await bucket.deleteFiles({prefix: `${userId}/`});
 
             resolve();
         } catch (err) {
