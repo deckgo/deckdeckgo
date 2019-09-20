1bc00bb8064a316df3c5f984805f1182cd5020cd
peterpeterparker <david.dalbusco@outlook.com>
2019-09-20 07:43:27 +0200

    feat(#260): init component

---
+++ webcomponents/color/src/components/color/deckdeckgo-color.tsx
@@ -0,0 +1,13 @@
+import {Component, h} from '@stencil/core';
+
+@Component({
+  tag: 'deckgo-color',
+  styleUrl: 'deckdeckgo-color.scss',
+  shadow: true
+})
+export class DeckdeckgoColor {
+
+  render() {
+    return <div>Color picker</div>;
+  }
+}
---
+++ webcomponents/color/src/components/color/readme.md
@@ -0,0 +1,10 @@
+# my-component
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
