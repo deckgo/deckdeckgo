70f3f97e19d47062f6e92548260d4fa34b8d2733
bokaderohit98 <bokaderohit98@gmail.com>
2019-10-21 14:07:03 +0530

    feat(studio): showing author social handles in author slide

--- studio/src/app/utils/editor/create-slides.utils.tsx
+++ studio/src/app/utils/editor/create-slides.utils.tsx
@@ -168,35 +168,35 @@ export class CreateSlidesUtils {
                 if (userSocial.twitter && userSocial.twitter !== '' && userSocial.twitter !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} twitter={user.data.social.twitter}>
                         <deckgo-lazy-img svg-src={`${config.globalAssetsUrl}/icons/ionicons/twitter.svg`} aria-label="Twitter"></deckgo-lazy-img>
-                        Twitter
+                        {userSocial.twitter}
                     </deckgo-social>);
                 }
 
                 if (userSocial.linkedin && userSocial.linkedin !== '' && userSocial.linkedin !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} linkedin={user.data.social.linkedin}>
                         <deckgo-lazy-img svg-src={`${config.globalAssetsUrl}/icons/ionicons/linkedin.svg`} aria-label="LinkedIn"></deckgo-lazy-img>
-                        LinkedIn
+                        {userSocial.linkedin}
                     </deckgo-social>);
                 }
 
                 if (userSocial.dev && userSocial.dev !== '' && userSocial.dev !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} dev={user.data.social.dev}>
                         <deckgo-lazy-img svg-src={`${config.globalAssetsUrl}/icons/dev.svg`} aria-label="Dev"></deckgo-lazy-img>
-                        Dev
+                        {userSocial.dev}
                     </deckgo-social>);
                 }
 
                 if (userSocial.medium && userSocial.medium !== '' && userSocial.medium !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} medium={user.data.social.medium}>
                         <deckgo-lazy-img svg-src={`${config.globalAssetsUrl}/icons/medium.svg`} aria-label="Medium"></deckgo-lazy-img>
-                        Medium
+                        {userSocial.medium}
                     </deckgo-social>);
                 }
 
                 if (userSocial.github && userSocial.github !== '' && userSocial.github !== undefined) {
                     links.push(<deckgo-social slot={`social-link`} github={user.data.social.github}>
                         <deckgo-lazy-img svg-src={`${config.globalAssetsUrl}/icons/ionicons/github.svg`} aria-label="GitHub"></deckgo-lazy-img>
-                        GitHub
+                        {userSocial.github}
                     </deckgo-social>);
                 }
 
