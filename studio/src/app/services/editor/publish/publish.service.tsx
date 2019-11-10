6acb876ee4d823ee46ebd8a9d21f5eb2429f3e69
peterpeterparker <david.dalbusco@outlook.com>
2019-11-10 17:29:02 +0100

    feat: const instead of let

--- studio/src/app/services/editor/publish/publish.service.tsx
+++ studio/src/app/services/editor/publish/publish.service.tsx
@@ -243,7 +243,7 @@ export class PublishService {
                 return;
             }
 
-            let result: string = content.replace(/<div slot="notes".*?>.*?<\/div>/gis, '');
+            const result: string = content.replace(/<div slot="notes".*?>.*?<\/div>/gis, '');
 
             resolve(result);
         });
