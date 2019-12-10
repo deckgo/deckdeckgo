29e3e93e8deb5a52bc3dff5d5a06d9212e2ec96a
peterpeterparker <david.dalbusco@outlook.com>
2019-12-10 11:59:31 +0100

    feat(#57): improve fade transition flash

--- webcomponents/core/src/components/utils/deckdeckgo-deck-transition.tsx
+++ webcomponents/core/src/components/utils/deckdeckgo-deck-transition.tsx
@@ -1,12 +1,16 @@
 import {h, FunctionalComponent} from '@stencil/core';
 
 export const HideSlides: FunctionalComponent = () => {
+
+  // Note: a start (hidden) opacity of 0 would has for effect to produce some "white flash" on transition.
+  // Setting a value like 0.4 reduce the flash effect and makes the transition smoother for my eyes.
+
   return (
     <style>{`
       ::slotted(.deckgo-slide-container) {
-        opacity: 0;
+        opacity: var(--transition-fade-hidden-opacity, 0.4);
         visibility: hidden;
-        transition: all var(--transition-fade-duration, 500ms);
+        transition: all var(--transition-fade-duration, 500ms) cubic-bezier(0.23, 1, 0.320, 1);
       };
     `}</style>
   );
