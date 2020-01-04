54f4f70f3d5fdb501b877430bfe9474e861f6999
peterpeterparker <david.dalbusco@outlook.com>
2020-01-04 10:46:10 +0100

    feat(#424): extract social to its own component

---
+++ webcomponents/social/CHANGELOG.md
@@ -0,0 +1,6 @@
+<a name="1.0.0-rc.1"></a>
+# 1.0.0-rc.1 (2020-01-04)
+
+### Features
+
+* initial version
---
+++ webcomponents/social/stencil.config.ts
@@ -0,0 +1,28 @@
+import { Config } from '@stencil/core';
+
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+
+export const config: Config = {
+  namespace: 'deckdeckgo-social',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ]
+};
