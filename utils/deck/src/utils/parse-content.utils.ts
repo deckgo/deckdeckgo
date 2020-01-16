c11eaa73de7a16132fc250c0d31885866dda2af5
peterpeterparker <david.dalbusco@outlook.com>
2020-01-16 08:05:47 +0100

    feat(#585): remove attribute custom-loader
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- utils/deck/src/utils/parse-content.utils.ts
+++ utils/deck/src/utils/parse-content.utils.ts
@@ -8,6 +8,7 @@ export function cleanContent(content: string): Promise<string> {
         let result: string = content.replace(/(<.*?)(contenteditable=""|contenteditable="true"|contenteditable="false"|contenteditable)(.*?>)/gi, '$1$3');
         result = result.replace(/(<.*?)(editable=""|editable="true"|editable)(.*?>)/gi, '$1$3');
         result = result.replace(/(<.*?)(highlighted=""|highlighted="true"|highlighted)(.*?>)/gi, '$1$3');
+        result = result.replace(/(<.*?)(custom-loader=""|custom-loader="true"|custom-loader)(.*?>)/gi, '$1$3');
         result = result.replace(/class="[a-zA-Z0-9:;\.\s\(\)\-\,]*"/gi, '');
 
         resolve(result);
