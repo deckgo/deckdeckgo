9630582ea38635a02220e14c2371366c4c7b2f58
peterpeterparker <david.dalbusco@outlook.com>
2020-02-07 17:32:00 +0100

    feat: new template to preserve ratio
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

---
+++ webcomponents/slides/aspect-ratio/src/components/deckdeckgo-slide-aspect-ratio.tsx
@@ -0,0 +1,91 @@
+import {Component, Element, Event, EventEmitter, Method, h, Host, Prop, State} from '@stencil/core';
+
+import {DeckdeckgoSlide, hideLazyLoadImages, afterSwipe, lazyLoadContent} from '@deckdeckgo/slide-utils';
+
+import {debounce} from '@deckdeckgo/utils';
+
+@Component({
+  tag: 'deckgo-slide-aspect-ratio',
+  styleUrl: 'deckdeckgo-slide-aspect-ratio.scss',
+  shadow: true
+})
+export class DeckdeckgoSlideAspectRatio implements DeckdeckgoSlide {
+  @Element() el: HTMLElement;
+
+  @Event() slideDidLoad: EventEmitter<void>;
+
+  @Prop()
+  ratio: number = 16 / 9;
+
+  @State()
+  private contentScale: number = 1;
+
+  async componentDidLoad() {
+    this.initWindowResize();
+
+    await hideLazyLoadImages(this.el);
+
+    this.slideDidLoad.emit();
+  }
+
+  private initWindowResize() {
+    if (window) {
+      window.addEventListener('resize', debounce(this.onResizeContent));
+    }
+  }
+
+  private onResizeContent = async () => {
+    this.contentScale = window.innerWidth / screen.width;
+  };
+
+  @Method()
+  beforeSwipe(_enter: boolean, _reveal: boolean): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      resolve(true);
+    });
+  }
+
+  @Method()
+  afterSwipe(): Promise<void> {
+    return afterSwipe();
+  }
+
+  @Method()
+  lazyLoadContent(): Promise<void> {
+    return lazyLoadContent(this.el);
+  }
+
+  @Method()
+  revealContent(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  @Method()
+  hideContent(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  render() {
+    return (
+      <Host
+        class={{
+          'deckgo-slide-container': true
+        }}
+        style={{'--content-scale': `${this.contentScale}`}}>
+        <div class="deckgo-slide">
+          <slot name="title"></slot>
+          <div class="deckgo-aspect-ratio-container">
+            <div class="deckgo-aspect-ratio-content">
+              <slot />
+            </div>
+            <slot name="header"></slot>
+            <slot name="footer"></slot>
+          </div>
+          <slot name="notes"></slot>
+          <slot name="actions"></slot>
+          <slot name="background"></slot>
+        </div>
+      </Host>
+    );
+  }
+}
---
+++ webcomponents/slides/aspect-ratio/src/components/readme.md
@@ -0,0 +1,51 @@
+# deckgo-slide-aspect-ratio
+
+<!-- Auto Generated Below -->
+
+## Properties
+
+| Property | Attribute | Description | Type     | Default  |
+| -------- | --------- | ----------- | -------- | -------- |
+| `ratio`  | `ratio`   |             | `number` | `16 / 9` |
+
+## Events
+
+| Event          | Description | Type                |
+| -------------- | ----------- | ------------------- |
+| `slideDidLoad` |             | `CustomEvent<void>` |
+
+## Methods
+
+### `afterSwipe() => Promise<void>`
+
+#### Returns
+
+Type: `Promise<void>`
+
+### `beforeSwipe(_enter: boolean, _reveal: boolean) => Promise<boolean>`
+
+#### Returns
+
+Type: `Promise<boolean>`
+
+### `hideContent() => Promise<void>`
+
+#### Returns
+
+Type: `Promise<void>`
+
+### `lazyLoadContent() => Promise<void>`
+
+#### Returns
+
+Type: `Promise<void>`
+
+### `revealContent() => Promise<void>`
+
+#### Returns
+
+Type: `Promise<void>`
+
+---
+
+_Built with [StencilJS](https://stenciljs.com/)_
