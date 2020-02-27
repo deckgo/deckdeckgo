eb7b0756f65c3605b1bc7e53d98da6d266072e78
peterpeterparker <david.dalbusco@outlook.com>
2020-02-27 09:43:05 +0100

    feat: remove label "Beta" in navigation bar
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- studio/src/app/components/core/app-navigation/app-navigation.tsx
+++ studio/src/app/components/core/app-navigation/app-navigation.tsx
@@ -60,9 +60,7 @@ export class AppNavigation {
         <ion-router-link onClick={() => this.closeMenu()} href="/" routerDirection="forward" class="home">
           <div>
             <app-logo></app-logo>
-            <ion-label class="ion-text-uppercase">
-              DeckDeckGo <mark>BETA</mark>
-            </ion-label>
+            <ion-label class="ion-text-uppercase">DeckDeckGo</ion-label>
           </div>
         </ion-router-link>
 
