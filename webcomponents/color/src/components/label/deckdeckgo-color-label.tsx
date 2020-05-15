be27138cad21b15784bc062deeb8ab44cc72f59e
peterpeterparker <david.dalbusco@outlook.com>
2020-05-15 07:52:24 +0200

    feat(#544): display color label in hex
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- webcomponents/color/src/components/label/deckdeckgo-color-label.tsx
+++ webcomponents/color/src/components/label/deckdeckgo-color-label.tsx
@@ -1,4 +1,4 @@
-import {Component, h, Prop} from '@stencil/core';
+import {Component, h, Prop, State, Watch} from '@stencil/core';
 
 @Component({
   tag: 'deckgo-color-label',
@@ -18,16 +18,56 @@ export class DeckdeckgoColorLabel {
   @Prop()
   customColorRgb: string;
 
+  @State()
+  private color: string;
+
+  async componentWillLoad() {
+    this.color = await this.initColorHex();
+  }
+
+  @Watch('colorHex')
+  @Watch('colorRgb')
+  @Watch('customColorRgb')
+  async watchColors() {
+    this.color = await this.initColorHex();
+  }
+
+  async initColorHex(): Promise<string> {
+    if (this.colorHex) {
+      return this.colorHex;
+    }
+
+    if (this.customColorRgb) {
+      return this.rgbToHex(this.customColorRgb);
+    }
+
+    return this.rgbToHex(this.colorRgb);
+  }
+
+  // https://stackoverflow.com/a/42429333/5404186
+  async rgbToHex(rgb: string | undefined): Promise<string | undefined> {
+    if (!rgb) {
+      return undefined;
+    }
+
+    const toHex = (rgb): string => `#${rgb.map((v) => v.toString(16).padStart(2, '0')).join('')}`;
+    const extractRgb = (rgb): number[] =>
+      rgb
+        .match(/(\d+),\s*(\d+),\s*(\d+)/)
+        .splice(1, 3)
+        .map((v) => Number(v));
+
+    return toHex(extractRgb(rgb));
+  }
+
   render() {
     if (!this.colorHex && !this.customColorRgb && !this.colorRgb) {
       return <p class="color-label"></p>;
     }
 
-    const color: string = `${this.colorHex ? this.colorHex : `rgb(${this.customColorRgb ? this.customColorRgb : this.colorRgb})`}`;
-
     return (
       <p class="color-label">
-        <span>{this.colorLabel ? this.colorLabel : <slot></slot>}</span> <small>{color}</small>
+        <span>{this.colorLabel ? this.colorLabel : <slot></slot>}</span> <small>{this.color}</small>
       </p>
     );
   }
