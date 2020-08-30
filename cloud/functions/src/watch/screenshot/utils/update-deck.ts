6c07e60fd9dc5ca269a23016b88e5d2ff3e08deb
peterpeterparker <david.dalbusco@outlook.com>
2020-08-30 13:55:54 +0200

    fix: firebase timestamp function not mapped

--- cloud/functions/src/watch/screenshot/utils/update-deck.ts
+++ cloud/functions/src/watch/screenshot/utils/update-deck.ts
@@ -40,5 +40,10 @@ function getDateObj(myDate: any): Date | null {
     return new Date(myDate.toDate());
   }
 
+  // A Firebase Timestamp format which for some reason has no mapped function
+  if (myDate && (myDate.seconds >= 0 || myDate.seconds < 0) && (myDate.nanoseconds >= 0 || myDate.nanoseconds < 0)) {
+    return new Date(myDate.seconds * 1000);
+  }
+
   return myDate;
 }
