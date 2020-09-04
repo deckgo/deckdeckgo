55120aef91709e64e7f3d2372aa911d02521cbdc
peterpeterparker <david.dalbusco@outlook.com>
2020-09-04 11:47:49 +0200

    feat: remove api presentation from studio, job is done in the cloud

--- studio/src/app/models/api/api.presentation.tsx
+++
@@ -1,4 +0,0 @@
-export interface ApiPresentation {
-  id: string;
-  url: string;
-}
--- studio/src/app/models/api/api.slide.tsx
+++
@@ -1,7 +0,0 @@
-import {SlideAttributes, SlideTemplate} from '../data/slide';
-
-export interface ApiSlide {
-  content?: string;
-  template: SlideTemplate;
-  attributes?: SlideAttributes;
-}
