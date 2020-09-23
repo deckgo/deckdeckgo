eca0abf9e8a0c690879e2cf90c95781735454efc
peterpeterparker <david.dalbusco@outlook.com>
2020-09-23 17:38:28 +0200

    feat: begin to introduce direction option

--- studio/src/app/components/editor/styles/deck/app-deck-transition/app-deck-transition.tsx
+++ studio/src/app/components/editor/styles/deck/app-deck-transition/app-deck-transition.tsx
@@ -15,6 +15,9 @@ export class AppDeckTransition {
   @State()
   private selectedTransition: 'slide' | 'fade' | 'none';
 
+  @State()
+  private selectedDirection: 'horizontal' | 'vertical' | 'papyrus';
+
   private timerInterval: NodeJS.Timeout;
   private timerCounter: number = 0;
 
@@ -64,37 +67,95 @@ export class AppDeckTransition {
     }, 2000);
   }
 
-  private applyTransition(transition: 'slide' | 'fade' | 'none'): Promise<void> {
-    return new Promise<void>((resolve) => {
-      if (!this.deckElement || !transition) {
-        resolve();
-        return;
-      }
+  private async applyDirection(direction: 'horizontal' | 'vertical' | 'papyrus') {
+    if (!this.deckElement || !direction) {
+      return;
+    }
 
-      this.deckElement.setAttribute('transition', transition);
+    this.deckElement.setAttribute('direction', direction);
 
-      this.transitionChange.emit();
+    this.transitionChange.emit();
+  }
 
-      resolve();
-    });
+  private async applyAnimation(animation: 'slide' | 'fade' | 'none') {
+    if (!this.deckElement || !animation) {
+      return;
+    }
+
+    this.deckElement.setAttribute('transition', animation);
+
+    this.transitionChange.emit();
   }
 
   render() {
+    return [this.renderDirection(), this.renderAnimation()];
+  }
+
+  private renderDirection() {
     return (
-      <div class="container ion-margin-top ion-margin-bottom">
-        {this.renderDeckItem('slide', 'var(--ion-color-primary)', 'Slide animation', this.selectedTransition === 'slide')}
+      <app-expansion-panel>
+        <ion-label slot="title">Direction</ion-label>
 
-        {this.renderDeckItem('fade', 'var(--ion-color-secondary)', 'Fade effect', this.selectedTransition === 'fade')}
+        <div class="device">
+          <ion-segment mode="ios" value="default">
+            <ion-segment-button value="default" mode="ios">
+              <ion-label>Default</ion-label>
+            </ion-segment-button>
+            <ion-segment-button value="mobile" mode="ios">
+              <ion-label>Mobile</ion-label>
+            </ion-segment-button>
+          </ion-segment>
+        </div>
 
-        {this.renderDeckItem('none', 'var(--ion-color-tertiary)', 'Instant transition', this.selectedTransition === 'none')}
-      </div>
+        <div class="container ion-margin-bottom">
+          {this.renderDeckItem('horizontal', this.selectedTransition, 'var(--ion-color-primary)', 'Horizontal', this.selectedDirection === 'horizontal', () =>
+            this.applyDirection('horizontal')
+          )}
+
+          {this.renderDeckItem('vertical', this.selectedTransition, 'var(--ion-color-secondary)', 'Vertical', this.selectedDirection === 'vertical', () =>
+            this.applyDirection('vertical')
+          )}
+
+          {this.renderDeckItem('papyrus', this.selectedTransition, 'var(--ion-color-tertiary)', 'Papyrus', this.selectedDirection === 'papyrus', () =>
+            this.applyDirection('papyrus')
+          )}
+        </div>
+      </app-expansion-panel>
     );
   }
 
-  private renderDeckItem(transition: 'slide' | 'fade' | 'none', nextSlideBackground: string, text: string, selected: boolean) {
+  private renderAnimation() {
     return (
-      <div class={`item ${transition} ${selected ? 'selected' : ''}`} custom-tappable onClick={() => this.applyTransition(transition)}>
-        <deckgo-deck embedded={true} keyboard={false} transition={transition}>
+      <app-expansion-panel>
+        <ion-label slot="title">Animation</ion-label>
+        <div class="container ion-margin-bottom">
+          {this.renderDeckItem(this.selectedDirection, 'slide', 'var(--ion-color-primary)', 'Slide animation', this.selectedTransition === 'slide', () =>
+            this.applyAnimation('slide')
+          )}
+
+          {this.renderDeckItem(this.selectedDirection, 'fade', 'var(--ion-color-secondary)', 'Fade effect', this.selectedTransition === 'fade', () =>
+            this.applyAnimation('fade')
+          )}
+
+          {this.renderDeckItem(this.selectedDirection, 'none', 'var(--ion-color-tertiary)', 'Instant transition', this.selectedTransition === 'none', () =>
+            this.applyAnimation('none')
+          )}
+        </div>
+      </app-expansion-panel>
+    );
+  }
+
+  private renderDeckItem(
+    direction: 'horizontal' | 'vertical' | 'papyrus',
+    transition: 'slide' | 'fade' | 'none',
+    nextSlideBackground: string,
+    text: string,
+    selected: boolean,
+    action: (selection) => Promise<void>
+  ) {
+    return (
+      <div class={`item ${selected ? 'selected' : ''}`} custom-tappable onClick={action}>
+        <deckgo-deck embedded={true} keyboard={false} transition={transition} direction={direction}>
           <deckgo-slide-title>
             <p slot="title">{text}</p>
           </deckgo-slide-title>
