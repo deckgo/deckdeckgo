b3e6091f559b95b0fde99e5f4d04344e59706b0c
Andreas Moldskred <moldskredandreas1@gmail.com>
2020-10-01 23:05:29 +0200

    Remove unwanted console.log

--- studio/src/app/helpers/editor/palette.helper.ts
+++ studio/src/app/helpers/editor/palette.helper.ts
@@ -1,7 +1,6 @@
 import {DeckdeckgoPalette, DeckdeckgoPaletteColor} from '@deckdeckgo/color';
 
 export function colorInPaletteHandler(currentPalette: DeckdeckgoPalette[], color: DeckdeckgoPaletteColor) {
-  console.log(currentPalette, color);
   if (currentPalette) {
     if (currentPalette.some((palette) => palette.color.hex === color.hex)) return currentPalette;
     return [{color}, ...currentPalette];
