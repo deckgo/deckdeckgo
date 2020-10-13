d2dda8bf6ebc3989133533e7bc762cb37cb597de
peterpeterparker <david.dalbusco@outlook.com>
2020-10-13 19:15:30 +0200

    feat: clean attribute editable when false too

--- utils/deck/src/utils/parse-content.utils.ts
+++ utils/deck/src/utils/parse-content.utils.ts
@@ -6,7 +6,7 @@ export function cleanContent(content: string): Promise<string> {
     }
 
     let result: string = content.replace(/(<.*?)(contenteditable=""|contenteditable="true"|contenteditable="false"|contenteditable)(.*?>)/gi, '$1$3');
-    result = result.replace(/(<.*?)(editable=""|editable="true"|editable)(.*?>)/gi, '$1$3');
+    result = result.replace(/(<.*?)(editable=""|editable="true"|editable="false"|editable)(.*?>)/gi, '$1$3');
     result = result.replace(/(<.*?)(highlighted=""|highlighted="true"|highlighted)(.*?>)/gi, '$1$3');
     result = result.replace(/(<.*?)(custom-loader=""|custom-loader="true"|custom-loader)(.*?>)/gi, '$1$3');
     result = result.replace(/class="[a-zA-Z0-9:;\.\s\(\)\-\,]*"/gi, '');
