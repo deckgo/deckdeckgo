6296f7cbbadb3f3d5bed937dd265492b74f1afb4
Christian Petro <1petrochristian@gmail.com>
2020-10-20 16:17:32 +0200

    Updated dependencies, added sass to component, updated stencil cofig file, and added prettierrc file

--- webcomponents/markdown-parser/stencil.config.ts
+++ webcomponents/markdown-parser/stencil.config.ts
@@ -1,5 +1,9 @@
 import { Config } from '@stencil/core';
 
+import {sass} from '@stencil/sass';
+import {postcss} from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+
 export const config: Config = {
   namespace: 'markdown-parser',
   outputTargets: [
@@ -8,14 +12,14 @@ export const config: Config = {
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+  ],
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
   ],
 };
