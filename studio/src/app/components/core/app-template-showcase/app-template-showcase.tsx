2bc0689fe60ef97863cefacc9ea976806614c9cf
peterpeterparker <david.dalbusco@outlook.com>
2020-12-07 17:56:39 +0100

    feat: display loader placeholder

--- studio/src/app/components/core/app-template-showcase/app-template-showcase.tsx
+++ studio/src/app/components/core/app-template-showcase/app-template-showcase.tsx
@@ -1,4 +1,4 @@
-import {Component, Prop, h} from '@stencil/core';
+import {Component, Prop, h, State} from '@stencil/core';
 
 import {Template, TemplateDataSlot} from '../../../models/data/template';
 
@@ -12,12 +12,17 @@ export class AppTemplateShowcase {
   @Prop()
   template: Template;
 
-  async componentWillLoad() {
+  @State()
+  private loaded: boolean = false;
+
+  async componentDidLoad() {
     await Utils.injectJS({
       id: `${this.template.data.tag}-script`,
       src: this.template.data.cdn,
       module: true,
     });
+
+    this.loaded = true;
   }
 
   private async blockSlide($event: CustomEvent) {
@@ -25,17 +30,29 @@ export class AppTemplateShowcase {
   }
 
   render() {
+    return <ion-card class="ion-no-margin">{this.loaded ? this.renderTemplate() : this.renderSpinner()}</ion-card>;
+  }
+
+  private renderTemplate() {
     const Element = this.template.data.tag;
+
     return (
-      <ion-card class="ion-no-margin">
-        <deckgo-deck embedded={true} keyboard={false} onSlidesDidLoad={($event: CustomEvent) => this.blockSlide($event)}>
-          <Element>
-            {this.template.data.slots.map((slot: TemplateDataSlot) => {
-              return <h1 slot={slot.name}>Hello</h1>;
-            })}
-          </Element>
-        </deckgo-deck>
-      </ion-card>
+      <deckgo-deck embedded={true} keyboard={false} onSlidesDidLoad={($event: CustomEvent) => this.blockSlide($event)}>
+        <Element>
+          {this.template.data.slots.map((slot: TemplateDataSlot) => {
+            return <h1 slot={slot.name}>Hello</h1>;
+          })}
+        </Element>
+      </deckgo-deck>
+    );
+  }
+
+  private renderSpinner() {
+    return (
+      <div class="spinner">
+        <ion-spinner color="medium"></ion-spinner>
+        <ion-label>Loading...</ion-label>
+      </div>
     );
   }
 }
