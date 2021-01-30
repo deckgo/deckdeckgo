bfdd006412b5e149a0c79ed7021099c0e68cfbe9
peterpeterparker <david.dalbusco@outlook.com>
2021-01-30 17:23:52 +0100

    feat: rotate

--- studio/src/app/components/editor/styles/element/app-block/app-block.tsx
+++ studio/src/app/components/editor/styles/element/app-block/app-block.tsx
@@ -27,6 +27,12 @@ export class AppBlock {
   @State()
   private paddingCSS: string;
 
+  @State()
+  private rotate: number;
+
+  @State()
+  private transformCSS: string;
+
   @Event() blockChange: EventEmitter<void>;
 
   private destroyListener;
@@ -38,15 +44,20 @@ export class AppBlock {
     await this.initPadding();
     await this.initPaddingCSS();
 
+    await this.initRotate();
+    await this.initTransformCSS();
+
     this.destroyListener = settingsStore.onChange('editMode', async (edit: EditMode) => {
       if (edit === 'css') {
         await this.initWidthCSS();
         await this.initPaddingCSS();
+        await this.initTransformCSS();
         return;
       }
 
       await this.initWidth();
       await this.initPadding();
+      await this.initRotate();
     });
   }
 
@@ -74,6 +85,16 @@ export class AppBlock {
 
   private async initWidthCSS() {
     this.widthCSS = this.selectedElement?.style.width;
+  }
+
+  private async initRotate() {
+    const matches: RegExpMatchArray | null = this.selectedElement?.style.transform.match(/(\d+)/);
+    const rotate: number = parseInt(matches?.[0]);
+    this.rotate = isNaN(rotate) ? 0 : rotate;
+  }
+
+  private async initTransformCSS() {
+    this.transformCSS = this.selectedElement?.style.transform;
   }
 
   private async updateWidth($event: CustomEvent<RangeChangeEventDetail>) {
@@ -105,7 +126,7 @@ export class AppBlock {
   }
 
   private async updatePadding($event: CustomEvent<RangeChangeEventDetail>) {
-    if (!$event || !$event.detail || $event.detail.value < 0 || $event.detail.value > 100) {
+    if (!$event || !$event.detail || $event.detail.value < 0) {
       return;
     }
 
@@ -132,6 +153,34 @@ export class AppBlock {
     this.blockChange.emit();
   }
 
+  private async updateRotate($event: CustomEvent<RangeChangeEventDetail>) {
+    if (!$event || !$event.detail || $event.detail.value < 0 || $event.detail.value > 360) {
+      return;
+    }
+
+    $event.stopPropagation();
+
+    this.rotate = $event.detail.value as number;
+
+    this.selectedElement.style.transform = `rotate(${this.rotate}deg)`;
+
+    this.blockChange.emit();
+  }
+
+  private handleTransformInput($event: CustomEvent<KeyboardEvent>) {
+    this.transformCSS = ($event.target as InputTargetEvent).value;
+  }
+
+  private updateTransformCSS() {
+    if (!this.selectedElement) {
+      return;
+    }
+
+    this.selectedElement.style.transform = this.transformCSS;
+
+    this.blockChange.emit();
+  }
+
   render() {
     return (
       <app-expansion-panel
@@ -141,8 +190,37 @@ export class AppBlock {
         <ion-list>
           {this.renderWidth()}
           {this.renderPadding()}
+          {this.renderRotate()}
         </ion-list>
       </app-expansion-panel>
+    );
+  }
+
+  private renderRotate() {
+    return (
+      <Fragment>
+        <ion-item-divider class="ion-padding-top">
+          <ion-label>Rotate {settingsStore.state.editMode === 'properties' ? <small>{this.rotate}deg</small> : undefined}</ion-label>
+        </ion-item-divider>
+
+        <ion-item class="item-opacity properties">
+          <ion-range
+            value={this.rotate}
+            min={0}
+            max={360}
+            mode="md"
+            onIonChange={($event: CustomEvent<RangeChangeEventDetail>) => this.updateRotate($event)}></ion-range>
+        </ion-item>
+
+        <ion-item class="with-padding css">
+          <ion-input
+            value={this.transformCSS}
+            placeholder="transform"
+            debounce={500}
+            onIonInput={(e: CustomEvent<KeyboardEvent>) => this.handleTransformInput(e)}
+            onIonChange={() => this.updateTransformCSS()}></ion-input>
+        </ion-item>
+      </Fragment>
     );
   }
 
@@ -154,12 +232,7 @@ export class AppBlock {
         </ion-item-divider>
 
         <ion-item class="item-opacity properties">
-          <ion-range
-            min={1}
-            max={100}
-            value={this.padding}
-            mode="md"
-            onIonChange={($event: CustomEvent<RangeChangeEventDetail>) => this.updatePadding($event)}></ion-range>
+          <ion-range value={this.padding} mode="md" onIonChange={($event: CustomEvent<RangeChangeEventDetail>) => this.updatePadding($event)}></ion-range>
         </ion-item>
 
         <ion-item class="with-padding css">
