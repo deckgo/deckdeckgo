41a153e45c75860f943697c3f7b1f3613c34cbc1
peterpeterparker <david.dalbusco@outlook.com>
2021-02-28 12:50:54 +0100

    feat: i18n links

--- studio/src/app/definitions/i18.d.ts
+++ studio/src/app/definitions/i18.d.ts
@@ -2,7 +2,27 @@ interface I18nCore {
   close: string;
 }
 
+interface I18nFooter {
+  home: string;
+  discover: string;
+  enterprise: string;
+  about: string;
+  team: string;
+  newsletter: string;
+  contact: string;
+  press: string;
+  faq: string;
+  developers: string;
+  open_source: string;
+  services: string;
+  developer: string;
+  terms: string;
+  terms_of_use: string;
+  privacy_policy: string;
+}
+
 interface I18n {
   lang: 'en';
   core: I18nCore;
+  footer: I18nFooter;
 }
