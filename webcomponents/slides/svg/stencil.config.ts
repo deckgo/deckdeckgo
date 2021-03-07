bbbe53638e6fb7fc0a1242626f0e93ccd2902e5d
peterpeterparker <david.dalbusco@outlook.com>
2021-03-07 11:57:04 +0100

    feat: template for fixed 16/9 svg

---
+++ webcomponents/slides/svg/stencil.config.ts
@@ -0,0 +1,29 @@
+import {Config} from '@stencil/core';
+
+import {sass} from '@stencil/sass';
+import {postcss} from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+
+export const config: Config = {
+  namespace: 'deckdeckgo-slide-svg',
+  outputTargets: [
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www',
+      serviceWorker: null
+    },
+    {
+      type: 'dist-custom-elements-bundle'
+    }
+  ],
+  plugins: [
+    sass({
+      includePaths: ['node_modules/@deckdeckgo/slide-utils/styles/']
+    }),
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ]
+};
