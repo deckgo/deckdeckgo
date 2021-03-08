795ba845c200e18f74948dbc4f2e8224ad4e0fc4
peterpeterparker <david.dalbusco@outlook.com>
2021-03-08 16:32:55 +0100

    feat: load font

--- cloud/functions/src/request/create.ts
+++ cloud/functions/src/request/create.ts
@@ -3,7 +3,9 @@ import fetch, {Response} from 'node-fetch';
 import * as puppeteer from 'puppeteer';
 
 export async function createDeck() {
-  const response: Response = await fetch(`https://raw.githubusercontent.com/deckgo/deckdeckgo/figma/webcomponents/slides/svg/showcase/slide-with-text.svg`);
+  const response: Response = await fetch(
+    `https://raw.githubusercontent.com/deckgo/deckdeckgo/figma/webcomponents/slides/svg/showcase/slide-with-text-without-font.svg`
+  );
 
   if (!response || !response.ok) {
     console.error(response);
@@ -19,9 +21,26 @@ export async function createDeck() {
   // Screenshot size
   await page.setViewport({width: 1980, height: 1080});
 
-  await page.setContent(`${svg}`, {waitUntil: 'domcontentloaded'});
+  const doc = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style>
+        @import url(https://fonts.googleapis.com/css?family=Signika);
+        body {
+          font-family: 'Signika', sans-serif;
+        }
+      </style>
+    </head>
+    <body>
+      ${svg}
+    </body>
+    </html>
+  `;
 
-  // await page.pdf({ path: 'html.pdf', format: 'A4' })
+  await page.goto('data:text/html;charset=UTF-8,' + encodeURIComponent(doc), {waitUntil: 'networkidle0'});
+
+  await page.pdf({path: 'html.pdf', format: 'A4'});
 
   // const text = await page.evaluate(() =>  document.querySelector('span'));
   const text = await page.mainFrame().$$('text');
