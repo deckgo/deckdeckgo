4c46425e2ba45564d1261f73b0aa22156c8baa3a
peterpeterparker <david.dalbusco@outlook.com>
2021-07-22 18:05:58 +0200

    chore: use ic for local development

--- studio/src/global/app-dev.ts
+++ studio/src/global/app-dev.ts
@@ -6,23 +6,12 @@ import {setupConfig as setupDeckGoConfig} from '../app/types/core/environment-co
 setupDeckGoConfig({
   deckdeckgo: {
     mock: true,
-    cloud: 'firebase',
+    cloud: 'ic',
     appUrl: 'http://localhost:3333',
     globalAssetsUrl: 'http://localhost:3333/assets',
     pollUrl: 'http://localhost:3333/poll',
     presentationUrl: '<@PRESENTATION_URL@>',
     socketUrl: '<@SOCKET_URL@>'
-  },
-  firebase: {
-    apiKey: '<@FIREBASE_API_KEY@>',
-    authDomain: '<@FIREBASE_AUTH_DOMAIN@>',
-    databaseURL: '<@FIREBASE_DATABASE_URL@>',
-    projectId: '<@FIREBASE_PROJECT_ID@>',
-    storageBucket: '<@FIREBASE_STORAGE_BUCKET@>',
-    messagingSenderId: '<@FIREBASE_MESSAGING_SENDER_ID@>',
-    storageUrl: '<@FIREBASE_STORAGE_URL@>',
-    functionsUrl: '<@FIREBASE_FUNCTIONS_URL@>',
-    appId: '<@FIREBASE_APP_ID@>'
   },
   google: {
     fontsUrl: 'https://fonts.googleapis.com/css?display=swap&family='
