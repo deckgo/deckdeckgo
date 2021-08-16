1853b8c6db520b9d0f607839fbc7f4ea69a4cffb
peterpeterparker <david.dalbusco@outlook.com>
2021-08-16 12:11:27 +0200

    feat: defer slides upload and delte to ic with web workers

--- studio/src/app/services/editor/sync/sync.ic.service.ts
+++ studio/src/app/services/editor/sync/sync.ic.service.ts
@@ -4,29 +4,17 @@ import {LocalStorage} from '@dfinity/auth-client';
 import authStore from '../../../stores/auth.store';
 import syncStore from '../../../stores/sync.store';
 
-import {SyncData, SyncDataSlide} from '../../../types/editor/sync';
-
-import {_SERVICE as DecskActor} from '../../../canisters/decks/decks.did';
+import {SyncData} from '../../../types/editor/sync';
 
 import {internetComputer} from '../../../utils/core/environment.utils';
-import {createDecksActor} from '../../../utils/core/ic.deck.utils';
 
 import {SyncService} from './sync.service';
-import {SlideIcService} from '../../data/slide/slide.ic.service';
 import {AuthFactoryService} from '../../auth/auth.factory.service';
 import {AuthIcService} from '../../auth/auth.ic.service';
 
 import {uploadWorker} from '../../../workers/sync.ic.worker';
 
 export class SyncIcService extends SyncService {
-  private slideIcService: SlideIcService;
-
-  constructor() {
-    super();
-
-    this.slideIcService = SlideIcService.getInstance();
-  }
-
   // @Override
   async upload(syncData: SyncData | undefined) {
     try {
@@ -66,42 +54,10 @@ export class SyncIcService extends SyncService {
         host: `${window.location}`
       });
 
-      const {updateSlides, deleteSlides} = syncData;
-
-      const decksActor: DecskActor = await createDecksActor({identity});
-
-      await this.uploadSlides({updateSlides, identity, decksActor});
-
-      await this.deleteSlides({deleteSlides, identity, decksActor});
-
-      // TODO: handle delete decks here?
-
       await this.clean(syncData);
     } catch (err) {
       syncStore.state.sync = 'error';
       console.error(err);
     }
-  }
-
-  private async uploadSlides({updateSlides, identity, decksActor}: {updateSlides: SyncDataSlide[] | undefined; identity: Identity; decksActor: DecskActor}) {
-    if (!updateSlides || updateSlides.length <= 0) {
-      return;
-    }
-
-    const promises: Promise<void>[] = updateSlides.map(({slide, deckId}: SyncDataSlide) =>
-      this.slideIcService.uploadSlide({slide, deckId, decksActor, identity})
-    );
-    await Promise.all(promises);
-  }
-
-  private async deleteSlides({deleteSlides, identity, decksActor}: {deleteSlides: SyncDataSlide[] | undefined; identity: Identity; decksActor: DecskActor}) {
-    if (!deleteSlides || deleteSlides.length <= 0) {
-      return;
-    }
-
-    const promises: Promise<void>[] = deleteSlides.map(({deckId, slideId}: SyncDataSlide) =>
-      this.slideIcService.deleteSlide({slideId, deckId, identity, decksActor})
-    );
-    await Promise.all(promises);
   }
 }
--- studio/src/app/workers/sync.ic.worker.ts
+++ studio/src/app/workers/sync.ic.worker.ts
@@ -6,7 +6,9 @@ import {_SERVICE as DecksActor, _SERVICE as DecskActor} from '../canisters/decks
 import {_SERVICE as DeckBucketActor} from '../canisters/deck/deck.did';
 
 import {Deck, DeckData} from '../models/data/deck';
-import {SyncData, SyncDataDeck} from '../types/editor/sync';
+import {Slide, SlideData} from '../models/data/slide';
+
+import {SyncData, SyncDataDeck, SyncDataSlide} from '../types/editor/sync';
 
 import {createDeckBucketActor, createDecksActor} from '../utils/core/ic.deck.utils';
 import {CanisterUtils} from '../utils/editor/canister.utils';
@@ -35,11 +37,17 @@ export const uploadWorker = async ({
 
   const identity: Identity = DelegationIdentity.fromDelegation(key, chain);
 
-  const {updateDecks} = syncData;
+  const {updateDecks, updateSlides, deleteSlides: slidesToDelete} = syncData;
 
   const decksActor: DecskActor = await createDecksActor({identity, host});
 
   await uploadDecks({updateDecks, identity, decksActor, host});
+
+  await uploadSlides({updateSlides, identity, decksActor, host});
+
+  await deleteSlides({deleteSlides: slidesToDelete, identity, decksActor, host});
+
+  // TODO: handle delete decks here?
 };
 
 const uploadDecks = async ({
@@ -89,4 +97,112 @@ const uploadDeck = async ({deck, decksActor, identity, host}: {deck: Deck; decks
 
   // TODO: remove, just for test
   console.log('Deck IC Get:', await deckBucket.get(), performance.now() - t2);
+};
+
+const uploadSlides = async ({
+  updateSlides,
+  identity,
+  decksActor,
+  host
+}: {
+  updateSlides: SyncDataSlide[] | undefined;
+  identity: Identity;
+  decksActor: DecskActor;
+  host: string;
+}) => {
+  if (!updateSlides || updateSlides.length <= 0) {
+    return;
+  }
+
+  const promises: Promise<void>[] = updateSlides.map(({slide, deckId}: SyncDataSlide) => uploadSlide({slide, deckId, decksActor, identity, host}));
+  await Promise.all(promises);
+};
+
+const deleteSlides = async ({
+  deleteSlides,
+  identity,
+  decksActor,
+  host
+}: {
+  deleteSlides: SyncDataSlide[] | undefined;
+  identity: Identity;
+  decksActor: DecskActor;
+  host: string;
+}) => {
+  if (!deleteSlides || deleteSlides.length <= 0) {
+    return;
+  }
+
+  const promises: Promise<void>[] = deleteSlides.map(({deckId, slideId}: SyncDataSlide) => deleteSlide({slideId, deckId, identity, decksActor, host}));
+  await Promise.all(promises);
+};
+
+const uploadSlide = async ({
+  slide,
+  deckId,
+  decksActor,
+  identity,
+  host
+}: {
+  slide: Slide;
+  deckId: string;
+  decksActor: DecksActor;
+  identity: Identity;
+  host: string;
+}) => {
+  if (!slide) {
+    return;
+  }
+
+  console.log('Slide IC about to SET');
+  const t0 = performance.now();
+
+  const bucket: Principal = await decksActor.init(deckId);
+
+  const deckBucket: DeckBucketActor = await createDeckBucketActor({identity, bucket, host});
+
+  await deckBucket.setSlide({
+    slideId: slide.id,
+    data: await CanisterUtils.toArray<SlideData>(slide.data),
+    created_at: CanisterUtils.toTimestamp((slide.data.created_at as Date) || new Date()),
+    updated_at: CanisterUtils.toTimestamp((slide.data.updated_at as Date) || new Date())
+  });
+
+  const t1 = performance.now();
+  console.log('Slide IC SET done', t1 - t0);
+
+  const t2 = performance.now();
+
+  // TODO: remove, just for test
+  console.log('Slide IC Get:', await deckBucket.getSlide(slide.id), performance.now() - t2);
+};
+
+const deleteSlide = async ({
+  slideId,
+  deckId,
+  decksActor,
+  identity,
+  host
+}: {
+  slideId: string;
+  deckId: string;
+  decksActor: DecksActor;
+  identity: Identity;
+  host: string;
+}) => {
+  if (!slideId) {
+    return;
+  }
+
+  console.log('Slide IC about to DEL');
+  const t0 = performance.now();
+
+  const bucket: Principal = await decksActor.init(deckId);
+
+  const deckBucket: DeckBucketActor = await createDeckBucketActor({identity, bucket, host});
+
+  await deckBucket.delSlide(slideId);
+
+  const t1 = performance.now();
+  console.log('Slide IC DEL done', t1 - t0);
 };
