181e3b7f820f60e75cafbc2a8eae6ef6c0e69028
David Dal Busco <david.dalbusco@outlook.com>
2021-09-04 09:25:55 +0200

    fix: slide navigation and selection from new aside menu (#1316)
    * fix: slide navigation from aside menu
    
    * refactor: use utils selector

--- studio/src/app/utils/editor/deck.utils.ts
+++ studio/src/app/utils/editor/deck.utils.ts
@@ -2,5 +2,13 @@ export const deckSelector: string = 'app-editor > ion-content div.deck > main > 
 
 export const slideTo = async (index: number) => {
   const deck: HTMLDeckgoDeckElement | undefined = document.querySelector(deckSelector);
-  await deck.slideTo(index);
+  await deck?.slideTo(index);
+};
+
+export const selectSlide = ({deck, index}: {deck: HTMLDeckgoDeckElement | null; index: number}): HTMLElement | null =>
+  deck?.querySelector(':scope > .deckgo-slide-container:nth-child(' + (index + 1) + ')');
+
+export const selectDeckSlide = (index: number): HTMLElement | null => {
+  const deck: HTMLDeckgoDeckElement | null = document.querySelector(deckSelector);
+  return selectSlide({deck, index});
 };
