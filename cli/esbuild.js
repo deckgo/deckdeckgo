02fb870a5ff9f909c788f41cf2e7d366a211cc4c
peterpeterparker <david.dalbusco@outlook.com>
2021-09-09 08:48:41 +0200

    build: cli workspace

--- cli/esbuild.js
+++ cli/esbuild.js
@@ -1,21 +1,17 @@
 const esbuild = require('esbuild');
 const {writeFile, mkdir} = require('fs').promises;
 
-const script = esbuild
-  .buildSync({
-      entryPoints: ['src/index.ts'],
-      bundle: true,
-      minify: true,
-      platform: 'node',
-      write: false,
-      target: ['node14']
-  });
+const script = esbuild.buildSync({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  minify: true,
+  platform: 'node',
+  write: false,
+  target: ['node14']
+});
 
 (async () => {
-      await mkdir('dist');
+  await mkdir('dist');
 
-      await writeFile(
-        'dist/index.js',
-        `#!/usr/bin/env node\n${script.outputFiles[0].text}`
-      );
+  await writeFile('dist/index.js', `#!/usr/bin/env node\n${script.outputFiles[0].text}`);
 })();
