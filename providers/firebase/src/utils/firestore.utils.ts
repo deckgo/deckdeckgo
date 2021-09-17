b471c4d48af9599c9d33f4db93c810888257c51f
peterpeterparker <david.dalbusco@outlook.com>
2021-09-17 08:07:03 +0200

    fix: firebase import

--- providers/firebase/src/utils/firestore.utils.ts
+++ providers/firebase/src/utils/firestore.utils.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export const filterFieldDelete = <T>(obj: T | null, replaceWithNull: boolean = false): T | null => {
   if (!obj) {
