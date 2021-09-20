74f0ab16f756cb5758b25b1a3a2b08956d8d226e
peterpeterparker <david.dalbusco@outlook.com>
2021-09-20 19:53:11 +0200

    build: deprecate firebase dev environment

--- studio/src/global/app-dev.ts
+++ studio/src/global/app-dev.ts
@@ -6,7 +6,7 @@ import {setupConfig as setupDeckGoConfig} from '../app/types/core/environment-co
 setupDeckGoConfig({
   app: {
     mock: true,
-    cloud: 'firebase'
+    cloud: 'offline'
   },
   deckdeckgo: {
     appUrl: 'http://localhost:3333',
@@ -20,16 +20,5 @@ setupDeckGoConfig({
   },
   google: {
     fontsUrl: 'https://fonts.googleapis.com/css?display=swap&family='
-  },
-  firebase: {
-    apiKey: '<@FIREBASE_API_KEY@>',
-    authDomain: '<@FIREBASE_AUTH_DOMAIN@>',
-    databaseURL: '<@FIREBASE_DATABASE_URL@>',
-    projectId: '<@FIREBASE_PROJECT_ID@>',
-    storageBucket: '<@FIREBASE_STORAGE_BUCKET@>',
-    messagingSenderId: '<@FIREBASE_MESSAGING_SENDER_ID@>',
-    storageUrl: '<@FIREBASE_STORAGE_URL@>',
-    functionsUrl: '<@FIREBASE_FUNCTIONS_URL@>',
-    appId: '<@FIREBASE_APP_ID@>'
   }
 });
