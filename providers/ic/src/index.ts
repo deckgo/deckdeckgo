2c65e61480af9361bf933dd5c4412833608d6727
peterpeterparker <david.dalbusco@outlook.com>
2021-09-22 13:07:06 +0200

    feat: extract ic to be implemented storage function

--- providers/ic/src/index.ts
+++ providers/ic/src/index.ts
@@ -5,3 +5,5 @@ export * from './providers/auth/auth.ic';
 export * from './providers/data/user.ic';
 export * from './providers/data/deck.ic';
 export * from './providers/data/slide.ic';
+
+export * from './providers/storage/storage.ic';
