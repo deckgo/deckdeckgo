2a7f90e06c0dcf22ce0feaee8d81350e7b5b8190
peterpeterparker <david.dalbusco@outlook.com>
2021-10-16 17:43:29 +0200

    fix: cmp tag

--- studio/src/app/pages/core/app-storage/app-storage.tsx
+++ studio/src/app/pages/core/app-storage/app-storage.tsx
@@ -74,7 +74,7 @@ export class AppStorage implements ComponentInterface {
       <Fragment>
         {this.renderFilter()}
 
-        <app-storage-files folder={this.folder}></app-storage-files>
+        <app-assets folder={this.folder}></app-assets>
       </Fragment>
     );
   }
