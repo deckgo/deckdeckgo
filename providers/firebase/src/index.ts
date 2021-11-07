808278b27d2ed92f9dc4d171de4b3dd8507cb1c9
peterpeterparker <david.dalbusco@outlook.com>
2021-11-07 14:32:54 +0100

    feat: export providers
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- providers/firebase/src/index.ts
+++ providers/firebase/src/index.ts
@@ -7,6 +7,8 @@ export * from './providers/data/template.firebase';
 export * from './providers/data/platform.firebase';
 export * from './providers/data/deck.firebase';
 export * from './providers/data/slide.firebase';
+export * from './providers/data/doc.firebase';
+export * from './providers/data/paragraph.firebase';
 
 export * from './providers/storage/storage.firebase';
 
