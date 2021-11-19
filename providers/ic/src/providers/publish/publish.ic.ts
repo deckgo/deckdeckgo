23913e3ef128d2e1310ed0ddb8765a3745491c5a
peterpeterparker <david.dalbusco@outlook.com>
2021-11-19 15:28:43 +0100

    feat: update index html to ic
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- providers/ic/src/providers/publish/publish.ic.ts
+++ providers/ic/src/providers/publish/publish.ic.ts
@@ -1,7 +1,15 @@
+import {Identity} from '@dfinity/agent';
+import {Principal} from '@dfinity/principal';
+
 import {Deck, DeckData, Publish, DeckPublishData, publishData} from '@deckdeckgo/editor';
 
-import {setData} from '../../utils/data.utils';
 import {_SERVICE as StorageBucketActor} from '../../canisters/storage/storage.did';
+
+import {getIdentity} from '../auth/auth.ic';
+
+import {setData} from '../../utils/data.utils';
+import {getStorageBucket} from '../../utils/manager.utils';
+import {upload} from '../../utils/storage.utils';
 
 export const publish: Publish = async ({deck: deckSource}: {deck: Deck; config: Record<string, string>}): Promise<Deck> => {
   const {id, data} = deckSource;
@@ -9,13 +17,37 @@ export const publish: Publish = async ({deck: deckSource}: {deck: Deck; config: 
   // 1. Update deck meta information
   const deck: Deck = await setData<Deck, DeckData>({key: `/decks/${id}`, id, data});
 
-  // 2. Trigger the function that effectively publish
-  await publishDeck({deck});
+  // 2. Init and fill HTML
+  const uploadData: {actor: StorageBucketActor; html: string; filename: string} = await initUpload({deck});
+
+  // 5. Upload
+  await uploadFileIC(uploadData);
 
   return deck;
 };
 
-const publishDeck = async ({deck}: {deck: Deck}): Promise<void> => {
+const initUpload = async ({deck}: {deck: Deck}): Promise<{actor: StorageBucketActor; html: string; filename: string}> => {
+  const {html, deckPublishData}: {html: string; deckPublishData: DeckPublishData} = await initIndexHTML({deck});
+
+  // 3. Get actor
+  const identity: Identity | undefined = getIdentity();
+  const {bucket, actor}: {bucket: Principal; actor: StorageBucketActor} = await getStorageBucket({identity});
+
+  // 4. Folder and filename
+  const folder: string = 'static';
+  const filename: string = encodeURI(deckPublishData.title);
+
+  // 4. Update URL
+  const indexHTML = html.replace('{{DECKDECKGO_URL}}', `https://${bucket.toText()}.raw.ic0.app/${folder}/${filename}`);
+
+  return {
+    html: indexHTML,
+    actor,
+    filename
+  };
+};
+
+const initIndexHTML = async ({deck}: {deck: Deck}): Promise<{html: string; deckPublishData: DeckPublishData}> => {
   const deckPublishData: DeckPublishData = publishData({deck});
 
   const template: string = await htmlTemplate();
@@ -26,7 +58,6 @@ const publishDeck = async ({deck}: {deck: Deck}): Promise<void> => {
     return acc;
   }, template);
 
-  // <deckgo-deck id="slider" embedded="true"> </deckgo-deck>
   const {attributes, slides} = deckPublishData;
 
   const attr: string = attributes
@@ -38,69 +69,22 @@ const publishDeck = async ({deck}: {deck: Deck}): Promise<void> => {
     `<deckgo-deck id="slider" embedded="true" ${attr || ''}>${slides.map((slide: string) => slide)}</deckgo-deck>`
   );
 
-  // TODO DECKDECKGO_BASE_HREF
-};
-
-const upload = async ({
-  data,
-  folder,
-  storageBucket
-}: {
-  data: File;
-  folder: string;
-  storageBucket: StorageBucketActor;
-}): Promise<{fullPath: string; filename: string; token: string}> => {
-  const filename: string = encodeURI(data.name);
-  const fullPath: string = `${folder}/${filename}`;
-  const token: string = uuid();
-
-  console.log('About to upload to the IC');
-  const t0 = performance.now();
-
-  const {batchId} = await storageBucket.create_batch({name: filename, fullPath, token, folder});
-
-  const t1 = performance.now();
-  console.log('Upload create_batch', t1 - t0);
-
-  const promises = [];
-
-  const chunkSize = 700000;
-
-  for (let start = 0; start < data.size; start += chunkSize) {
-    const chunk: Blob = data.slice(start, start + chunkSize);
-
-    promises.push(
-      uploadChunk({
-        batchId,
-        chunk,
-        storageBucket
-      })
-    );
-  }
-
-  const chunkIds: {chunkId: bigint}[] = await Promise.all(promises);
-
-  const t2 = performance.now();
-  console.log('Upload upload chunks', t2 - t1);
-
-  await storageBucket.commit_batch({
-    batchId,
-    chunkIds: chunkIds.map(({chunkId}: {chunkId: bigint}) => chunkId),
-    contentType: data.type
-  });
-
-  const t3 = performance.now();
-  console.log('Upload commit_batch', t3 - t2);
-  console.log('Data uploaded', t3 - t0);
-
   return {
-    fullPath,
-    filename,
-    token
+    html: updatedTemplate,
+    deckPublishData
   };
 };
 
 const htmlTemplate = async (): Promise<string> => {
   const htmlTemplate: Response = await fetch('https://raw.githubusercontent.com/deckgo/ic-kit/main/dist/deck.html');
   return htmlTemplate.text();
+};
+
+const uploadFileIC = async ({filename, html, actor}: {filename: string; html: string; actor: StorageBucketActor}): Promise<void> => {
+  await upload({
+    data: new Blob([JSON.stringify(html)], {type: 'application/json; charset=utf-8'}),
+    filename,
+    folder: 'static',
+    storageBucket: actor
+  });
 };
