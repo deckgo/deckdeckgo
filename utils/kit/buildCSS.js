6a2a9d40a1b24aa3c5225307f8fd84432ce7db74
peterpeterparker <david.dalbusco@outlook.com>
2021-12-16 17:33:36 +0100

    feat: generate split css files
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- utils/kit/buildCSS.js
+++ utils/kit/buildCSS.js
@@ -1,36 +1,44 @@
 #!/usr/bin/env node
 
-const {writeFile, mkdir, rm} = require('fs').promises;
+const {writeFile, mkdir, rm, readdir} = require('fs').promises;
+const path = require('path');
 
 const {renderSync} = require('node-sass');
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const CleanCSS = require('clean-css');
 
+const generateCSS = async ({fileName}) => {
+  const {css, map} = renderSync({
+    file: `./styles/${fileName}.scss`,
+    outFile: `./css/${fileName}.css`,
+    outputStyle: 'expanded',
+    sourceMap: true
+  });
+
+  const {css: prefixedCSS} = await postcss([autoprefixer]).process(css, {
+    from: undefined,
+    to: `./css/${fileName}.css`,
+    map: false
+  });
+
+  await writeFile(`./css/${fileName}.css`, prefixedCSS);
+  await writeFile(`./css/${fileName}.css.map`, map);
+
+  const {styles: minifyCSS} = new CleanCSS().minify(prefixedCSS);
+
+  await writeFile(`./css/${fileName}.min.css`, minifyCSS);
+};
+
 (async () => {
   try {
     await rm('./css', {recursive: true, force: true});
     await mkdir('./css', {recursive: true});
 
-    const {css, map} = renderSync({
-      file: './styles/index.scss',
-      outFile: './css/kit.css',
-      outputStyle: 'expanded',
-      sourceMap: true
-    });
+    const files = await readdir('./styles/');
 
-    const {css: prefixedCSS} = await postcss([autoprefixer]).process(css, {
-      from: undefined,
-      to: './css/kit.css',
-      map: false
-    });
-
-    await writeFile('./css/kit.css', prefixedCSS);
-    await writeFile('./css/kit.css.map', map);
-
-    const {styles: minifyCSS} = new CleanCSS().minify(prefixedCSS);
-
-    await writeFile('./css/kit.min.css', minifyCSS);
+    const promises = files.map((file) => generateCSS({fileName: path.parse(file).name}));
+    await Promise.all(promises);
 
     console.log(`CSS generated!`);
   } catch (err) {
