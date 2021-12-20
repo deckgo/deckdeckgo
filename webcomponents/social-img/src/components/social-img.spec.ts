a469ca0352aa2ea270675f2ded428f6bee473c2d
peterpeterparker <david.dalbusco@outlook.com>
2021-12-20 16:43:14 +0100

    test: update according last changes
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- webcomponents/social-img/src/components/social-img.spec.ts
+++ webcomponents/social-img/src/components/social-img.spec.ts
@@ -25,7 +25,7 @@ describe('social-image', () => {
         <mock:shadow-root>
           <svg height="628px" width="1200px" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
             <rect fill="#3dc2ff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="80" y="80"></rect>
-            <rect fill="white" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
+            <rect fill="#ffffff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
           </svg>
         </mock:shadow-root>
       </deckgo-social-img>
@@ -43,7 +43,7 @@ describe('social-image', () => {
         <mock:shadow-root>
           <svg height="628px" width="1200px" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
             <rect fill="#3dc2ff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="80" y="80"></rect>
-            <rect fill="white" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
+            <rect fill="#ffffff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
             <foreignObject height="436" width="1008" x="96" xmlns="http://www.w3.org/1999/xhtml" y="96"><p part="text">${text}</p></foreignObject>
           </svg>
         </mock:shadow-root>
@@ -62,7 +62,7 @@ describe('social-image', () => {
         <mock:shadow-root>
           <svg height="628px" width="1200px" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
             <rect fill="#3dc2ff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="80" y="80"></rect>
-            <rect fill="white" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
+            <rect fill="#ffffff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
             <image height="64" href="${imgParsed}" part="img" width="64" x="1040" y="468"></image>
           </svg>
         </mock:shadow-root>
@@ -81,8 +81,8 @@ describe('social-image', () => {
         <mock:shadow-root>
           <svg height="628px" width="1200px" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
             <rect fill="#3dc2ff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="80" y="80"></rect>
-            <rect fill="white" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
-            <foreignObject height="436" width="912" x="96"  xmlns="http://www.w3.org/1999/xhtml" y="96"><p part="text">${text}</p></foreignObject>
+            <rect fill="#ffffff" height="500" rx="0" ry="0" stroke="#3dc2ff" stroke-width="5" width="1072" x="64" y="64"></rect>
+            <foreignObject height="436" width="912" x="96" y="96"><p part="text">${text}</p></foreignObject>
             <image height="64" href="${imgParsed}" part="img" width="64" x="1040" y="468"></image>
           </svg>
         </mock:shadow-root>
