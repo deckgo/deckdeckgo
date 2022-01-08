3cae6a134e460e18dd37bd57533df00335418a32
David Dal Busco <david.dalbusco@outlook.com>
2022-01-08 08:54:46 +0100

    build: utils cjs (#1449)
    * build: cjs output
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * chore: release utils v4
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * build: bump prettier and esbuild
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- utils/utils/esbuild.js
+++ utils/utils/esbuild.js
@@ -12,3 +12,15 @@ esbuild
     target: ['esnext']
   })
   .catch(() => process.exit(1));
+
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'lib/index.cjs.js',
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    platform: 'node',
+    target: ['node16']
+  })
+  .catch(() => process.exit(1));
