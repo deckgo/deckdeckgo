e9b208fda4b58a503866bd790c6f3b18fa0cb60a
peterpeterparker <david.dalbusco@outlook.com>
2022-01-28 16:47:44 +0100

    refactor: extract code menu
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

---
+++ studio/src/app/menus/code.menu.ts
@@ -0,0 +1,21 @@
+import {StyloMenu} from '../../../../../stylo';
+
+export const codeMenu: StyloMenu = {
+  match: ({paragraph}: {paragraph: HTMLElement}) => paragraph?.nodeName.toLowerCase() === 'deckgo-highlight-code',
+  actions: [
+    {
+      text: 'edit_code',
+      icon: `<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 512 512'>
+            <polygon points='364.13 125.25 87 403 64 448 108.99 425 386.75 147.87 364.13 125.25' style='fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/>
+            <path d='M420.69,68.69,398.07,91.31l22.62,22.63,22.62-22.63a16,16,0,0,0,0-22.62h0A16,16,0,0,0,420.69,68.69Z' style='fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/>
+          </svg>`,
+      action: async ({paragraph}: {paragraph: HTMLElement}) => {
+        const editCode: CustomEvent<void> = new CustomEvent<void>('editCode', {
+          bubbles: true
+        });
+
+        paragraph.dispatchEvent(editCode);
+      }
+    }
+  ]
+};
