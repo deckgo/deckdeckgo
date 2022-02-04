c1cbeba58d52dcf97e0f5936e63f650a69f62256
David Dal Busco <david.dalbusco@outlook.com>
2022-02-04 10:45:04 +0100

    feat: style code in docs (#1462)
    * feat: edit code style on doc
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * feat: exclude attributes
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * feat: filter attributes for update paragraphs
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- studio/src/app/config/editor.ts
+++ studio/src/app/config/editor.ts
@@ -10,5 +10,6 @@ import {codeMenu} from '../menus/code.menu';
 
 export const editorConfig: Partial<StyloConfig> = {
   plugins: [h1, h2, h3, ul, imgStorage, imgUnsplash, imgGif, code, hr],
-  menus: [codeMenu]
+  menus: [codeMenu],
+  excludeAttributes: ['id', 'hydrated', 'editable', 'paragraph_id', 'highlighted', 'custom-loader']
 };
