7176d2da812461ac9bc39b9391a8c1dc30533204
David Dal Busco <david.dalbusco@outlook.com>
2022-02-18 10:02:38 +0100

    feat: escape code characters (#1476)
    * feat: escape unsafe characters
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * chore: release monaco editor v1.1.0
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>
    
    * build: update monaco editor
    
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- webcomponents/monaco-editor/CHANGELOG.md
+++ webcomponents/monaco-editor/CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.1.0 (2022-02-18)
+
+### Features
+
+- escape code characters
+
 # 1.0.0 (2022-01-23)
 
 ### Features
---
+++ webcomponents/monaco-editor/src/utils/code.utils.ts
@@ -0,0 +1,10 @@
+export const escapeCode = (code: string | undefined): string | undefined =>
+  code?.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
+
+export const unescapeCode = (code: string | undefined): string | undefined =>
+  code
+    ?.replace(/'&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'");
