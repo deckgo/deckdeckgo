31257a38763083e0b9bd7c33c54797f97b108b24
peterpeterparker <david.dalbusco@outlook.com>
2022-02-20 17:40:43 +0100

    feat: extract collect data
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- webcomponents/studio/src/index.ts
+++ webcomponents/studio/src/index.ts
@@ -18,3 +18,4 @@ export * from './utils/parse-elements.utils';
 export * from './utils/parse-paragraphs.utils';
 export * from './utils/slot.utils';
 export * from './utils/sync.utils';
+export * from './utils/sync.worker.utils';
---
+++ webcomponents/studio/src/utils/sync.worker.utils.ts
@@ -0,0 +1,142 @@
+import {
+  Deck,
+  Doc,
+  Paragraph,
+  Slide,
+  SyncData,
+  SyncDataDeck,
+  SyncDataDoc,
+  SyncDataParagraph,
+  SyncDataSlide,
+  SyncPending,
+  SyncPendingData,
+  SyncPendingDeck,
+  SyncPendingDoc,
+  SyncPendingParagraph,
+  SyncPendingSlide
+} from '@deckdeckgo/editor';
+import {get, getMany} from 'idb-keyval';
+
+export const collectData = async (): Promise<SyncData | undefined> => {
+  const data: SyncPending | undefined = await get<SyncPending>('deckdeckgo_pending_sync');
+
+  if (!data) {
+    return undefined;
+  }
+
+  const syncedAt: Date = new Date();
+
+  const decksData: Partial<SyncData> = await collectDecksData(data);
+  const docsData: Partial<SyncData> = await collectDocsData(data);
+
+  return {
+    ...decksData,
+    ...docsData,
+    syncedAt
+  } as SyncData;
+};
+
+const collectDecksData = async (data: SyncPending): Promise<Partial<SyncData>> => {
+  const updateDecks: SyncDataDeck[] | undefined = (
+    await getMany(uniqueSyncData(data.updateDecks).map(({key}: SyncPendingDeck) => key))
+  ).map((deck: Deck) => ({
+    deckId: deck.id,
+    deck
+  }));
+
+  const deleteDecks: SyncDataDeck[] | undefined = uniqueSyncData(data.deleteDecks).map(({deckId}: SyncPendingDeck) => ({deckId}));
+
+  const updateSlides: SyncDataSlide[] | undefined = await Promise.all(
+    uniqueSyncData(data.updateSlides).map((slide: SyncPendingSlide) => getSlide(slide))
+  );
+
+  const deleteSlides: SyncDataSlide[] | undefined = uniqueSyncData(data.deleteSlides).map(({deckId, slideId}: SyncPendingSlide) => ({
+    deckId,
+    slideId
+  }));
+
+  return {
+    updateDecks: deleteDecks
+      ? updateDecks?.filter(
+          ({deckId}: SyncDataDeck) => !deleteDecks.find(({deckId: deleteDeckId}: SyncDataDeck) => deleteDeckId === deckId)
+        )
+      : updateDecks,
+    deleteDecks,
+    updateSlides: deleteSlides
+      ? updateSlides?.filter(
+          ({slideId}: SyncDataSlide) => !deleteSlides.find(({slideId: deleteSlideId}: SyncDataSlide) => deleteSlideId === slideId)
+        )
+      : updateSlides,
+    deleteSlides
+  };
+};
+
+const collectDocsData = async (data: SyncPending): Promise<Partial<SyncData>> => {
+  const updateDocs: SyncDataDoc[] | undefined = (await getMany(uniqueSyncData(data.updateDocs).map(({key}: SyncPendingDoc) => key))).map(
+    (doc: Doc) => ({
+      docId: doc.id,
+      doc
+    })
+  );
+
+  const deleteDocs: SyncDataDoc[] | undefined = uniqueSyncData(data.deleteDocs).map(({docId}: SyncPendingDoc) => ({docId}));
+
+  const updateParagraphs: SyncDataParagraph[] | undefined = await Promise.all(
+    uniqueSyncData(data.updateParagraphs).map((paragraph: SyncPendingParagraph) => getParagraph(paragraph))
+  );
+
+  const deleteParagraphs: SyncDataParagraph[] | undefined = uniqueSyncData(data.deleteParagraphs).map(
+    ({docId, paragraphId}: SyncPendingParagraph) => ({
+      docId,
+      paragraphId
+    })
+  );
+
+  return {
+    updateDocs: deleteDocs
+      ? updateDocs?.filter(({docId}: SyncDataDoc) => !deleteDocs.find(({docId: deleteDocId}: SyncDataDoc) => deleteDocId === docId))
+      : updateDocs,
+    deleteDocs,
+    updateParagraphs: deleteParagraphs
+      ? updateParagraphs?.filter(
+          ({paragraphId}: SyncDataParagraph) =>
+            !deleteParagraphs.find(({paragraphId: deleteParagraphId}: SyncDataParagraph) => deleteParagraphId === paragraphId)
+        )
+      : updateParagraphs,
+    deleteParagraphs
+  };
+};
+
+const getSlide = async ({deckId, slideId, key}: SyncPendingSlide): Promise<SyncDataSlide> => {
+  const slide: Slide | undefined = await get(key);
+
+  return {
+    deckId,
+    slideId,
+    slide
+  };
+};
+
+const getParagraph = async ({docId, paragraphId, key}: SyncPendingParagraph): Promise<SyncDataParagraph> => {
+  const paragraph: Paragraph | undefined = await get(key);
+
+  return {
+    docId,
+    paragraphId,
+    paragraph
+  };
+};
+
+const uniqueSyncData = (data: SyncPendingData[] | undefined): SyncPendingData[] => {
+  return (data || []).reduce((acc: SyncPendingData[], curr: SyncPendingData) => {
+    const index: number = acc.findIndex(({key}: SyncPendingData) => key === curr.key);
+
+    if (index === -1) {
+      acc.push(curr);
+    } else if (acc[index].queuedAt.getTime() > curr.queuedAt.getTime()) {
+      acc[index] = curr;
+    }
+
+    return acc;
+  }, []);
+};
