28c40f40287ed86f6787df6085a4569dc7e0f129
peterpeterparker <david.dalbusco@outlook.com>
2022-02-22 17:39:52 +0100

    feat: remove console.log
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- webcomponents/studio/src/components/indicator/indicator.tsx
+++ webcomponents/studio/src/components/indicator/indicator.tsx
@@ -24,9 +24,6 @@ export class Indicator implements ComponentInterface {
   }
 
   render() {
-
-    console.log('here',busyStore.state.busy,  i18n.state.indicator.saving, busyStore.state.docReady)
-
     return (
       <Host>
         {busyStore.state.busy ? i18n.state.indicator.saving : ''}
