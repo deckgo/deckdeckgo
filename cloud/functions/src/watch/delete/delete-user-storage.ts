fc9db444ebfa7e9368f10f9a8446e2cb73935760
peterpeterparker <david.dalbusco@outlook.com>
2022-03-11 10:30:36 +0100

    chore: format
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- cloud/functions/src/watch/delete/delete-user-storage.ts
+++ cloud/functions/src/watch/delete/delete-user-storage.ts
@@ -1,33 +1,33 @@
 import * as admin from 'firebase-admin';
 
 export async function deleteUserStorage(userRecord: admin.auth.UserRecord) {
-    if (!userRecord || !userRecord.uid || userRecord.uid === undefined || userRecord.uid === '') {
-        return;
-    }
+  if (!userRecord || !userRecord.uid || userRecord.uid === undefined || userRecord.uid === '') {
+    return;
+  }
 
-    try {
-        const userId: string = userRecord.uid;
+  try {
+    const userId: string = userRecord.uid;
 
-        await deleteStorage(userId);
-    } catch (err) {
-        console.error(err);
-    }
+    await deleteStorage(userId);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function deleteStorage(userId: string | null): Promise<void> {
-    return new Promise<void>(async (resolve, reject) => {
-        try {
-            if (!userId || userId === undefined || userId === '') {
-                resolve();
-                return;
-            }
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      if (!userId || userId === undefined || userId === '') {
+        resolve();
+        return;
+      }
 
-            const bucket = admin.storage().bucket();
-            await bucket.deleteFiles({prefix: `${userId}/`});
+      const bucket = admin.storage().bucket();
+      await bucket.deleteFiles({prefix: `${userId}/`});
 
-            resolve();
-        } catch (err) {
-            reject(err);
-        }
-    });
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
