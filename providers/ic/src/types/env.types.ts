44a5d33c8970844451dfcf866e00ad6dcd361bf4
peterpeterparker <david.dalbusco@outlook.com>
2022-03-20 21:16:28 +0100

    refactor: move ic providers to papyrs
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- providers/ic/src/types/env.types.ts
+++
@@ -1,6 +0,0 @@
-export interface EnvironmentIC extends Record<string, string | boolean> {
-  managerCanisterId: string;
-  localIdentityCanisterId?: string;
-  kitPath: string;
-  author: string;
-}
